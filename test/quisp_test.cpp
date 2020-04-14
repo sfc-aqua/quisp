447eabd9c5661b3bb32cc1a1791febf557a5dd3b
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-04-15 01:45:39 +0900

    basic workflow

--- test/quisp_test.cpp
+++ test/quisp_test.cpp
@@ -1,7 +1,20 @@
+#include<iostream>
+#include<fstream>
 #include<gtest/gtest.h>
 
+
 TEST(NoError, Test){
-    float fidelity;
-    fidelity = 1.0;
-    EXPECT_EQ(fidelity, 1.0);
+float fidelity;
+fidelity = 1.0;
+EXPECT_EQ(fidelity, 1.0);
 }
+
+// int main(){
+//     std::ifstream ifs("./testresult.txt");
+//     int line_num = 1;
+//     std::string line;
+//     while (getline(ifs,line)){
+//         std::cout << line_num << ":" << line << std::endl;
+//         line_num += 1;
+//   }
+// }
--- test/test.sh
+++ test/test.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 # run the result_test.ini in the network. This is not an unit test.
-pwd
+echo "test start"
 cd /root/quisp/quisp/networks
 make -C ../ cleanall
-make -C ../ BATCH=true QCONFIG=/root/quisp/quisp/network/* all
+make -C ../ BATCH=true QCONFIG=/root/quisp/quisp/network/* -j $(nproc) all
 # These are creating all simulations HACK
 # if we set quisp as ./quisp/..., may be error because of existance of the same name binary 
-/root/quisp/quisp/out/clang-release/quisp  -u Cmdenv -c Test_Realistic_Layer2_Simple  -f /root/quisp/quisp/networks/test.ini >> testresults.txt
-echo "here at $(pwd)"
+if [ -e /root/quisp/test/testresults.txt ];then
+    rm  /root/quisp/test/testresults.txt
+else
+    touch /root/quisp/test/testresults.txt
+fi
+# Actual test part
+# ==== This should be iterative
+echo "init" >> /root/quisp/test/testresults.txt 
+/root/quisp/quisp/out/clang-release/quisp  -u Cmdenv --cmdenv-express-mode=true -c Test_Realistic_Layer2_Simple_NoError  -f /root/quisp/quisp/networks/test.ini | grep "fidelity" | tr ";" "\n">> /root/quisp/test/testresults.txt
+echo " ">> /root/quisp/test/testresults.txt 
+/root/quisp/quisp/out/clang-release/quisp  -u Cmdenv --cmdenv-express-mode=true -c Test_Realistic_Layer2_Simple  -f /root/quisp/quisp/networks/test.ini | grep "fidelity" | tr ";" "\n">> /root/quisp/test/testresults.txt
+echo "next round">>/root/quisp/test/testresults.txt
+# ==== 
 # On docker
-g++ -std=c++11 -o quisp_test /root/quisp/test/quisp_test.cpp -I/rooot/omnetpp/include -I/usr/include/eigen3 -I/root/clibrary/googletest/googletest/include -L/root/clibrary/googletest/build/lib/ -pthread -lgtest_main -lgtest
+g++ -std=c++11 -o /root/quisp/test/quisp_test /root/quisp/test/quisp_test.cpp -I/rooot/omnetpp/include -I/usr/include/eigen3 -I/root/clibrary/googletest/googletest/include -L/root/clibrary/googletest/build/lib/ -pthread -lgtest_main -lgtest
 # remove used results
-./quisp_test
+/root/quisp/test/quisp_test
 echo "test done"
