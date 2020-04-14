ce86c2e62cae89cc1d1682f87e63f33c6eb0be12
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-04-14 19:36:00 +0900

    add test

--- test/test_docker_run.sh
+++ test/test_docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -it --name quisp -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh -c "cd test;sh test.sh;" 
+docker run --rm -it --name quisp -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh -c "sh test.sh;" 
