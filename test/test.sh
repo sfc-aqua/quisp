5b99c03c9db055dd66481a3653abe0ba600a1154
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-04-14 20:00:50 +0900

    solve path

--- test/test.sh
+++ test/test.sh
@@ -5,9 +5,9 @@ make -C ../quisp cleanall
 make -C ../quisp BATCH=true QCONFIG=../quisp/network/ all
 # These are creating all simulations HACK
 # if we set quisp as ./quisp/..., may be error because of existance of the same name binary 
-cd ../quisp && ./out/clang-release/quisp  -u Cmdenv -c Test_Realistic_Layer2_Simple  -f ./networks/test.ini > ../test/testresults.txt
+cd ../quisp && ./out/clang-release/quisp  -u Cmdenv -c Test_Realistic_Layer2_Simple  -f ./networks/test.ini > testresults.txt
 echo "here at $(pwd)"
 # On docker
-g++ -std=c++11 ./quisp_test.cpp -I../../omnetpp/include -I../../clibrary/googletest/googletest/include -L../../clibrary/googletest/build/lib/ -pthread -lgtest_main -lgtest
+g++ -std=c++11 ./test/quisp_test.cpp -I../omnetpp/include -I../clibrary/googletest/googletest/include -L../clibrary/googletest/build/lib/ -pthread -lgtest_main -lgtest
 # remove used results
 echo "test done"
