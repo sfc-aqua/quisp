14f8cac69f3305ea2fe30e6c615464c8638b5ad5
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-04-15 21:53:45 +0900

    check files

--- test_docker_run.sh
+++ test_docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run --rm -it --name quisp -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh -c "/bin/bash　/root/quisp/test/test.sh"
+docker run --rm -it --name quisp -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh -c "cd /root/quisp/test/; /bin/bash test.sh"
 # docker exec -it quisp /bin/bash
