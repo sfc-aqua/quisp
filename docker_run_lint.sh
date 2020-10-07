860ccf36d9805dc4a42fad8fd573c0347f80a7b9
zigen <hrlclb@gmail.com>
2020-10-08 00:00:04 +0900

    add slim docker image for ci

--- docker_run_lint.sh
+++ docker_run_lint.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
-if [ "$TRAVIS" == "true" ] ; then
+#!/bin/bash
+if [ "$TRAVIS" = "true" ]; then
     DIFF=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
-    echo $DIFF
     SRCS=$(echo $DIFF | tr " " "\n" | grep 'quisp/.*.cc$' | tr "\n" " ")
     HEADERS=$(echo $DIFF | tr " " "\n" | grep 'quisp/.*.h$' | tr "\n" ",")
     OPTS="-O3 -DNDEBUG=1 -MMD -MP -MF tidy.d -fPIC -Wno-deprecated-register -Wno-unused-function -fno-omit-frame-pointer -DHAVE_SWAPCONTEXT -DXMLPARSER=libxml -DPREFER_QTENV -DWITH_QTENV -DWITH_PARSIM -DWITH_NETBUILDER -DWITH_OSGEARTH -I/usr/include/eigen3 -I./quisp -I/root/omnetpp/include"
 
-    if [ "$SRCS" == "" ]; then 
+    if [ "$SRCS" = "" ]; then 
         echo "no change"
         exit 0
     fi
 
-docker run --rm  -i --name quisp -e TRAVIS -e TRAVIS_COMMIT_RANGE -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh <<-EOF
+docker run --rm  -i --name quisp -e TRAVIS -e TRAVIS_COMMIT_RANGE -v "$TRAVIS_BUILD_DIR:/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh <<-EOF
     cd /root/quisp
     clang-tidy -warnings-as-errors="*" -header-filter="$HEADERS" $SRCS -- $OPTS
 EOF
