8f8f516c5f33c700e037ca686f6d295f773bdc47
zigen <hrlclb@gmail.com>
2021-04-26 18:48:04 +0900

    add Interface class for RealTimeController

---
+++ quisp/modules/QRSA/RealTimeController/IRealTimeController.h
@@ -0,0 +1,27 @@
+#ifndef QUISP_MODULES_IREALTIMECONTROLLER_H_
+#define QUISP_MODULES_IREALTIMECONTROLLER_H_
+
+#include <classical_messages_m.h>
+#include <modules/QNIC.h>
+#include <omnetpp.h>
+#include <vector>
+
+using namespace omnetpp;
+
+namespace quisp {
+namespace modules {
+
+class IRealTimeController : public cSimpleModule {
+ protected:
+  virtual void initialize() = 0;
+  virtual void handleMessage(cMessage* msg) = 0;
+  virtual cModule* getQNode() = 0;
+
+ public:
+  virtual void EmitPhoton(int qnic_index, int qubit_index, QNIC_type qnic_type, int pulse) = 0;
+  virtual void ReInitialize_StationaryQubit(int qnic_index, int qubit_index, QNIC_type qnic_type, bool consumed) = 0;
+};
+}  // namespace modules
+}  // namespace quisp
+
+#endif /* QUISP_MODULES_IREALTIMECONTROLLER_H_ */
