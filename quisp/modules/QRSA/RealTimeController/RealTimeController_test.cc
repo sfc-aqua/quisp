f913303fe7cfa8b76a38b8672e2dd8690f4d5b9e
zigen <hrlclb@gmail.com>
2021-04-26 18:48:46 +0900

    temp: add RealTimeController test, but it raises segmentation fault...

--- quisp/modules/QRSA/RealTimeController/RealTimeController_test.cc
+++ quisp/modules/QRSA/RealTimeController/RealTimeController_test.cc
@@ -2,5 +2,23 @@
 #include "gtest/gtest.h"
 
 namespace {
-TEST(RealTimeControllerTest, Init) { quisp::modules::RealTimeController controller(); }
+
+class RTCTestTarget : public quisp::modules::RealTimeController {
+ public:
+  using quisp::modules::RealTimeController::initialize;
+  using quisp::modules::RealTimeController::par;
+  RTCTestTarget() {
+    omnetpp::cParImpl* p = new omnetpp::cIntParImpl();
+    const char *name = "address";
+    p->setName(name);
+    p->setIntValue(123);
+    this->addPar(p);
+  }
+};
+
+TEST(RealTimeControllerTest, Init) {
+  RTCTestTarget c{};
+  c.initialize();
+  ASSERT_EQ(c.par("address").intValue(), 123);
+}
 }  // namespace
