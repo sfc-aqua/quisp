0c7a538a1bf1b677934b3ccdaf6ac2fbaf20fd0b
PorametPat <benz19582@hotmail.com>
2021-06-08 11:16:15 +0700

    remove comment

--- quisp/modules/QRSA/ConnectionManager/ConnectionManager.cc
+++ quisp/modules/QRSA/ConnectionManager/ConnectionManager.cc
@@ -23,7 +23,7 @@ void ConnectionManager::initialize() {
   hardware_monitor = provider.getHardwareMonitor();
   my_address = par("address");
   num_of_qnics = par("total_number_of_qnics");
-  simultaneous_ES = true;//par("simultaneous_ES");
+  simultaneous_ES = par("simultaneousES"); // true;
 
   for (int i = 0; i < num_of_qnics; i++) {
     // qnode address
@@ -319,7 +319,7 @@ void ConnectionManager::respondToRequest(ConnectionSetupRequest *req) {
         if (!simultaneous_ES){
           SwappingConfig config = generateSwappingConfig(path.at(i), path, swapping_partners, qnics, num_resource);
           RuleSet *rule = generateEntanglementSwappingRuleSet(path.at(i), config);
-          //error("We F up again");
+          
           
           ConnectionSetupResponse *pkr = new ConnectionSetupResponse("ConnSetupResponse(Swapping)");
           pkr->setDestAddr(path.at(i));
@@ -331,9 +331,9 @@ void ConnectionManager::respondToRequest(ConnectionSetupRequest *req) {
           send(pkr, "RouterPort$o");
         }
         else if(simultaneous_ES){
-          //error("We F up again but in simulES");
+          
           SwappingConfig config = generateSimultaneousSwappingConfig(path.at(i), path, qnics, num_resource);
-          //error("We F up again but in simulES");
+          
           RuleSet *rule = generateSimultaneousEntanglementSwappingRuleSet(path.at(i), config, path);
           
           ConnectionSetupResponse *pkr = new ConnectionSetupResponse("ConnSetupResponse(Swapping)");
