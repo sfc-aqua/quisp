faaa73013ffb9214e8b78704cea934a69ab9abbf
Poramet <benz19582@hotmail.com>
2021-06-20 22:36:14 +0700

    Adding message type to router

--- quisp/modules/Common/Router.cc
+++ quisp/modules/Common/Router.cc
@@ -144,6 +144,10 @@ void Router::handleMessage(cMessage *msg) {
     bubble("Swapping Result packet received");
     send(pk, "rePort$o");
     return;
+  } else if (destAddr == myAddress && dynamic_cast<SimultaneousSwappingResult *>(msg) != nullptr) {
+    bubble("Swapping Result packet received");
+    send(pk, "rePort$o");
+    return;
   } else if (destAddr == myAddress && dynamic_cast<LinkTomographyRequest *>(msg) != nullptr) {
     bubble("Link tomography request received");
     send(pk, "hmPort$o");
