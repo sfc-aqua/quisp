0dafb87ba481eba0a3a7b6553106e19ba1a78928
Chibikuri <ryosuke.satoh.wk@gmail.com>
2021-06-21 10:27:54 +0900

    test constructor for ruleengine, but not working?

--- quisp/modules/QRSA/RuleEngine/RuleEngine_test.cc
+++ quisp/modules/QRSA/RuleEngine/RuleEngine_test.cc
@@ -23,23 +23,45 @@ class MockStationaryQubit : public StationaryQubit {
   MOCK_METHOD(void, setFree, (bool consumed), (override));
 };
 
+
+
+class MockRoutingDaemon : public IRoutingDaemon {
+ public:
+  MOCK_METHOD(int, return_QNIC_address_to_destAddr, (int destAddr), (override));
+};
+
+class MockHardwareMonitor : public IHardwareMonitor{
+  public:
+    MOCK_METHOD(std::unique_ptr<ConnectionSetupInfo>, findConnectionInfoByQnicAddr, (int qnic_address), (override));
+};
+
 class Strategy : public quisp_test::TestComponentProviderStrategy {
  public:
-  Strategy() : mockQubit(nullptr) {}
-  Strategy(MockStationaryQubit* _qubit) : mockQubit(_qubit) {}
-  ~Strategy() { delete mockQubit; }
+  Strategy() : mockQubit(nullptr), routingDaemon(nullptr), hardwareMonitor(nullptr) {}
+  Strategy(MockStationaryQubit* _qubit,
+           MockRoutingDaemon* routing_daemon,
+           MockHardwareMonitor* hardware_monitor) : mockQubit(_qubit), routingDaemon(routing_daemon), hardwareMonitor(hardware_monitor) {}
+  ~Strategy() { 
+    delete mockQubit;
+    delete routingDaemon;
+    delete hardwareMonitor;
+  }
   MockStationaryQubit* mockQubit = nullptr;
+  MockRoutingDaemon*  routingDaemon = nullptr;
+  MockHardwareMonitor* hardwareMonitor = nullptr;
   StationaryQubit* getStationaryQubit(int qnic_index, int qubit_index, QNIC_type qnic_type) override {
     if (mockQubit == nullptr) mockQubit = new MockStationaryQubit();
     return mockQubit;
   };
+  IRoutingDaemon getRoutingDaemon() override {return routingDaemon;};
+  IHardwareMonitor getHardwareMonitor() override {return hardwareMonitor;};
 };
 
 class RuleEngineTestTarget : public quisp::modules::RuleEngine {
  public:
   using quisp::modules::RuleEngine::initialize;
   using quisp::modules::RuleEngine::par;
-  RuleEngineTestTarget(MockStationaryQubit* mockQubit) : quisp::modules::RuleEngine() {
+  RuleEngineTestTarget(MockStationaryQubit* mockQubit, MockRoutingDaemon* routing_daemon, MockHardwareMonitor hardware_monitor) : quisp::modules::RuleEngine() {
     setParInt(this, "address", 123);
     setParInt(this, "number_of_qnics_rp", 0);
     setParInt(this, "number_of_qnics_r", 0);
@@ -55,14 +77,31 @@ class RuleEngineTestTarget : public quisp::modules::RuleEngine {
     // setParInt(this, "num_measure", 0);
 
     this->setName("rule_engine_test_target");
-    this->provider.setStrategy(std::make_unique<Strategy>(mockQubit));
+    this->provider.setStrategy(std::make_unique<Strategy>(mockQubit, routing_daemon, hardware_monitor));
   }
+  private:
+    FRIEND_TEST(RuleEngineTest, ESResourceUpdate);
 };
 
 TEST(RuleEngineTest, Init) {
   RuleEngineTestTarget c{nullptr};
   c.initialize();
   ASSERT_EQ(c.par("address").intValue(), 123);
+}
+
+TEST(RuleEngineTest, ESResourceUpdate){
+  // test for resource update in entanglement swapping
+  auto *routingdaemon = new MockRoutingDaemon;
+  auto *mockHardwareMonitor = new MockHardwareMonitor;
+  auto *mockQubit = new MockStationaryQubit;
+  RuleEngineTestTarget c{mockRoutingDaemon, mockHardwareMonitor, mockQubit};
+  EXPECT_CALL(*routingdaemon, return_QNIC_address_to_destAddr()).WillOnce(Return(2)).WillOnce(Return(1));
+  EXPECT_CALL(*mockHardwareMonitor, findConnectionInfoByQnicAddr()).WillOnce(Return(1));
+  // EXPECT_CALL(*mockQubit, returnNumEndNodes()).WillOnce(Return(1));
+  c.initialize();
+  swapping_result swapr;
+  swapr.new_partner = 1;
+  c.updateResources_EntanglementSwapping(swapr);
 }
 
 }  // namespace
