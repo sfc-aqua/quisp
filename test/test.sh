c27064e23f7e3351ce819d23549392cd2156026b
zigen <hrlclb@gmail.com>
2021-07-20 23:47:18 +0900

    fix test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,15 +2,16 @@
 set -eu
 # run the result_test.ini in the network. This is not an unit test.
 echo "test start"
-cd /root/quisp
+QUISP_ROOT=`pwd`/..
+cd $QUISP_ROOT
 make makefile-exe
-cd /root/quisp/quisp/
+cd $QUISP_ROOT/quisp/
 make cleanall
-make BATCH=true QCONFIG=/root/quisp/quisp/networks/* -j $(nproc) all
+make BATCH=true QCONFIG=$QUISP_ROOT/quisp/networks/* -j $(nproc) all
 # These are creating all simulations HACK
 # if we set quisp as ./quisp/..., may be error because of existance of the same name binary
-if [ -e /root/quisp/test/testresults.txt ];then
-    rm  /root/quisp/test/testresults.txt
+if [ -e $QUISP_ROOT/test/testresults.txt ];then
+    rm  $QUISP_ROOT/quisp/test/testresults.txt
 fi
 # Actual test part
 # Experiments=("Test0" "Test1" "Test2")
@@ -20,8 +21,8 @@ NUM_TEST=35
 # for((i=0; i<${#Experiments[@]}; i++)); do
 for((i=0; i<$NUM_TEST; i++)); do
 echo "start test $i"
-echo "init$i" >> /root/quisp/test/testresults.txt
-/root/quisp/quisp/out/clang-release/quisp  -u Cmdenv --cmdenv-express-mode=true -c "Test$i" -f /root/quisp/quisp/networks/test.ini | grep "fidelity" | tr ";" "\n" | tr "{" "\n"| tr "}" "\n"|sed s/"<-->"/"\n"/g>> /root/quisp/test/testresults.txt
-echo "next">>/root/quisp/test/testresults.txt
+echo "init$i" >> $QUISP_ROOT/test/testresults.txt
+$QUISP_ROOT/quisp/out/clang-release/quisp  -u Cmdenv --cmdenv-express-mode=true -c "Test$i" -f $QUISP_ROOT/quisp/quisp/networks/test.ini | grep "fidelity" | tr ";" "\n" | tr "{" "\n"| tr "}" "\n"|sed s/"<-->"/"\n"/g>> $QUISP_ROOT/test/testresults.txt
+echo "next">>$QUISP_ROOT/test/testresults.txt
 echo "finish test $i"
 done
