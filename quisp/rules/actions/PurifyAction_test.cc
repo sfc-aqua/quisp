422efafbbe9fb5716b30d7bb808edae7d558927d
zigen <hrlclb@gmail.com>
2021-08-18 20:48:19 +0900

    add PurifyAction constructor test

--- quisp/rules/actions/PurifyAction_test.cc
+++ quisp/rules/actions/PurifyAction_test.cc
@@ -22,7 +22,17 @@ using OriginalPurifyAction = quisp::rules::actions::PurifyAction;
 namespace {
 class PurifyAction : public OriginalPurifyAction {
  public:
+  using OriginalPurifyAction::num_purify;
+  using OriginalPurifyAction::partner;
   using OriginalPurifyAction::PurifyAction;
+  using OriginalPurifyAction::qnic_id;
+  using OriginalPurifyAction::qnic_type;
+  using OriginalPurifyAction::resource;
+  using OriginalPurifyAction::rule_id;
+  using OriginalPurifyAction::ruleset_id;
+  using OriginalPurifyAction::trash_resource;
+  using OriginalPurifyAction::X;
+  using OriginalPurifyAction::Z;
 };
 
 TEST(PurifyActionTest, Init) {
@@ -33,8 +43,22 @@ TEST(PurifyActionTest, Init) {
   int trash_resource_index = 5;
   unsigned long ruleset_id = 6;
   unsigned long rule_id = 7;
+  bool x_purification_enabled = true;
+  bool z_purification_enabled = true;
+  int num_purification = 8;
 
-  auto *action = new PurifyAction(partner_addr, qnic_type, qnic_id, resource_index, trash_resource_index, ruleset_id, rule_id);
+  auto *action = new PurifyAction(ruleset_id, rule_id, x_purification_enabled, z_purification_enabled, num_purification, partner_addr, qnic_type, qnic_id, resource_index,
+                                  trash_resource_index);
+  EXPECT_EQ(action->partner, partner_addr);
+  EXPECT_EQ(action->qnic_type, QNIC_E);
+  EXPECT_EQ(action->qnic_id, qnic_id);
+  EXPECT_EQ(action->resource, resource_index);
+  EXPECT_EQ(action->trash_resource, trash_resource_index);
+  EXPECT_EQ(action->ruleset_id, ruleset_id);
+  EXPECT_EQ(action->rule_id, rule_id);
+  EXPECT_EQ(action->X, x_purification_enabled);
+  EXPECT_EQ(action->Z, z_purification_enabled);
+  EXPECT_EQ(action->num_purify, num_purification);
 }
 
 TEST(PurifyActionTest, runXPurify) { auto *sim = prepareSimulation(); }
