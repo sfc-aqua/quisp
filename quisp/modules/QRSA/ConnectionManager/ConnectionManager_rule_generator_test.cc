111d2825e67f5a533c55dae034d4e294b1b1f615
zigen <hrlclb@gmail.com>
2021-08-19 13:58:48 +0900

    add ConnectionManager tests

--- quisp/modules/QRSA/ConnectionManager/ConnectionManager_rule_generator_test.cc
+++ quisp/modules/QRSA/ConnectionManager/ConnectionManager_rule_generator_test.cc
@@ -2,6 +2,10 @@
 #include <gtest/gtest.h>
 #include <omnetpp.h>
 #include "ConnectionManager.h"
+#include "gtest/gtest_prod.h"
+#include "modules/QNIC.h"
+#include "modules/QRSA/ConnectionManager/IConnectionManager.h"
+#include "rules/Action.h"
 #include "test_utils/TestUtils.h"
 
 namespace {
@@ -16,6 +20,9 @@ class Strategy : public quisp_test::TestComponentProviderStrategy {
 class ConnectionManager : public quisp::modules::ConnectionManager {
  public:
   using quisp::modules::ConnectionManager::par;
+  using quisp::modules::ConnectionManager::purificationRule;
+  using quisp::modules::ConnectionManager::swappingRule;
+
   ConnectionManager() : quisp::modules::ConnectionManager() {
     setParInt(this, "address", 123);
     setParInt(this, "total_number_of_qnics", 10);
@@ -25,12 +32,57 @@ class ConnectionManager : public quisp::modules::ConnectionManager {
 };
 
 TEST(ConnectionManagerRuleGenTest, PurificationRule) {
-  ConnectionManager c;
-  ASSERT_EQ(c.par("address").intValue(), 123);
+  ConnectionManager connection_manager;
+  int partner_addr = 1;
+  int purification_type = 0;  // 0: X, 1: Z
+  int num_purification = 3;
+  QNIC_type qnic_type = QNIC_E;
+  int qnic_id = 4;
+  unsigned long ruleset_id = 5;
+  unsigned long rule_id = 6;
+  std::unique_ptr<Rule> rule;
+  rule = connection_manager.purificationRule(partner_addr, purification_type, num_purification, qnic_type, qnic_id, ruleset_id, rule_id);
+  ASSERT_NE(rule, nullptr);
+  ASSERT_NE(rule->condition, nullptr);
+
+  ASSERT_NE(rule->action, nullptr);
+  ASSERT_NE(dynamic_cast<PurifyAction*>(rule->action.get()), nullptr);
+  EXPECT_EQ(rule->ruleset_id, ruleset_id);
+  EXPECT_EQ(rule->rule_index, rule_id);
+  // now action's ruleset_id and rule_id are not used.
+  // EXPECT_EQ(rule->action->ruleset_id, ruleset_id);
+  // EXPECT_EQ(rule->action->rule_id, rule_id);
+  EXPECT_EQ(rule->name, "X purification with : 1");
+
+  purification_type = 1;
+  rule = connection_manager.purificationRule(partner_addr, purification_type, num_purification, qnic_type, qnic_id, ruleset_id, rule_id);
+  ASSERT_NE(rule, nullptr);
+  ASSERT_NE(rule->condition, nullptr);
+  ASSERT_NE(rule->action, nullptr);
+  ASSERT_NE(dynamic_cast<PurifyAction*>(rule->action.get()), nullptr);
+  EXPECT_EQ(rule->ruleset_id, ruleset_id);
+  EXPECT_EQ(rule->rule_index, rule_id);
+  // EXPECT_EQ(rule->action->ruleset_id, ruleset_id);
+  // EXPECT_EQ(rule->action->rule_id, rule_id);
+  EXPECT_EQ(rule->name, "Z purification with : 1");
 }
 
 TEST(ConnectionManagerRuleGenTest, SwappingRule) {
-  ConnectionManager c;
-  ASSERT_EQ(c.par("address").intValue(), 123);
+  ConnectionManager connection_manager;
+  unsigned long ruleset_id = 5;
+  unsigned long rule_id = 6;
+  SwappingConfig conf{.left_partner = 4, .right_partner = 7};
+  std::unique_ptr<Rule> rule;
+  rule = connection_manager.swappingRule(conf, ruleset_id, rule_id);
+  ASSERT_NE(rule, nullptr);
+  ASSERT_NE(rule->condition, nullptr);
+
+  ASSERT_NE(rule->action, nullptr);
+  ASSERT_NE(dynamic_cast<SwappingAction*>(rule->action.get()), nullptr);
+  EXPECT_EQ(rule->ruleset_id, ruleset_id);
+  EXPECT_EQ(rule->rule_index, rule_id);
+  EXPECT_EQ(rule->action->ruleset_id, ruleset_id);
+  EXPECT_EQ(rule->action->rule_id, rule_id);
+  EXPECT_EQ(rule->name, "Entanglement Swapping with 4 : 7");
 }
 }  // namespace
