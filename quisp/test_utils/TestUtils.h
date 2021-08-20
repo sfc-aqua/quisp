57bf8e671334b7be7717e4b9337cf850c84634f9
zigen <hrlclb@gmail.com>
2021-08-20 17:30:14 +0900

    add mocks

--- quisp/test_utils/TestUtils.h
+++ quisp/test_utils/TestUtils.h
@@ -7,13 +7,17 @@
 #include "StaticEnv.h"
 #include "TestComponentProviderStrategy.h"
 #include "UtilFunctions.h"
+#include "mock_modules/MockHardwareMonitor.h"
 #include "mock_modules/MockQubit.h"
+#include "mock_modules/MockRoutingDaemon.h"
 #include "mock_modules/MockRuleEngine.h"
 
 namespace quisp_test {
 
 // use these functions and classes in your unit test.
 using gate::TestGate;
+using mock_modules::hardware_monitor::MockHardwareMonitor;
+using mock_modules::routing_daemon::MockRoutingDaemon;
 using mock_modules::rule_engine::MockRuleEngine;
 using mock_modules::stationary_qubit::MockQubit;
 using module_type::TestModuleType;
