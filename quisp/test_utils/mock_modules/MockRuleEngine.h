8426be90a532fa6c317f8493161ece5b9bbdd5e4
zigen <hrlclb@gmail.com>
2021-08-20 17:30:14 +0900

    add mocks

--- quisp/test_utils/mock_modules/MockRuleEngine.h
+++ quisp/test_utils/mock_modules/MockRuleEngine.h
@@ -3,12 +3,13 @@
 #include <gmock/gmock.h>
 #include <modules/QRSA/RuleEngine/IRuleEngine.h>
 
-using quisp::modules::IRuleEngine;
-using quisp::modules::IStationaryQubit;
-
 namespace quisp_test {
 namespace mock_modules {
 namespace rule_engine {
+
+using quisp::modules::IRuleEngine;
+using quisp::modules::IStationaryQubit;
+
 class MockRuleEngine : public IRuleEngine {
  public:
   MOCK_METHOD(void, freeResource, (int, int, QNIC_type), (override));
