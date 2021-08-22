fca227eb40e92b39fd4764f63e5ae45f2533ae16
zigen <hrlclb@gmail.com>
2021-08-22 16:44:08 +0900

    temp: add es with purify ruleset tests

--- quisp/modules/QRSA/ConnectionManager/ConnectionManager_test.cc
+++ quisp/modules/QRSA/ConnectionManager/ConnectionManager_test.cc
@@ -4,6 +4,9 @@
 #include <omnetpp.h>
 #include "modules/QNIC.h"
 #include "modules/QRSA/HardwareMonitor/IHardwareMonitor.h"
+#include "rules/Action.h"
+#include "rules/Clause.h"
+#include "rules/clauses/EnoughResourceClause.h"
 #include "test_utils/TestUtils.h"
 
 namespace {
@@ -26,7 +29,8 @@ class ConnectionManagerTestTarget : public quisp::modules::ConnectionManager {
  public:
   using quisp::modules::ConnectionManager::par;
   using quisp::modules::ConnectionManager::respondToRequest;
-  ConnectionManagerTestTarget(IRoutingDaemon *routing_daemon, IHardwareMonitor *hardware_monitor) : quisp::modules::ConnectionManager() {
+  ConnectionManagerTestTarget(IRoutingDaemon *routing_daemon, IHardwareMonitor *hardware_monitor)
+      : quisp::modules::ConnectionManager(), toRouterGate(new TestGate(this, "RouterPort$o")) {
     setParInt(this, "address", 123);
     setParInt(this, "total_number_of_qnics", 10);
     this->setName("connection_manager_test_target");
@@ -44,6 +48,13 @@ class ConnectionManagerTestTarget : public quisp::modules::ConnectionManager {
     this->provider.setStrategy(std::make_unique<Strategy>());
     setComponentType(new module_type::TestModuleType("test cm"));
   }
+  cGate *gate(const char *gatename, int index = -1) override {
+    if (strcmp(gatename, "RouterPort$o") != 0) {
+      throw cRuntimeError("unknown gate called");
+    }
+    return toRouterGate;
+  };
+  TestGate *toRouterGate;
 };
 
 TEST(ConnectionManagerTest, Init) {
@@ -55,16 +66,22 @@ TEST(ConnectionManagerTest, RespondToRequest) {
   auto *sim = prepareSimulation();
   auto *routing_daemon = new MockRoutingDaemon();
   auto *hardware_monitor = new MockHardwareMonitor();
-  ConnectionManagerTestTarget connection_manager(routing_daemon, hardware_monitor);
-  connection_manager.par("address") = 5;
-  connection_manager.callInitialize();
+  auto *connection_manager = new ConnectionManagerTestTarget(routing_daemon, hardware_monitor);
+  sim->registerComponent(connection_manager);
+  connection_manager->par("address") = 5;
+  connection_manager->callInitialize();
   auto *req = new ConnectionSetupRequest;
+
   // [QNode2](qnic_addr:1) -- (2)[QNode3](3) -- (4)[QNode4](5) -- (6)[QNode5(test target)]
   req->setActual_destAddr(5);
   req->setActual_srcAddr(4);
   req->setDestAddr(5);
   req->setSrcAddr(2);
-  req->setStack_of_QNICsArraySize(4);
+  req->setStack_of_QNICsArraySize(3);
+  req->setStack_of_QNodeIndexesArraySize(3);
+  req->setStack_of_QNodeIndexes(0, 2);
+  req->setStack_of_QNodeIndexes(1, 3);
+  req->setStack_of_QNodeIndexes(2, 4);
   req->setStack_of_QNICs(0, QNIC_id_pair{.fst = NULL_CONNECTION_SETUP_INFO.qnic, .snd = {.type = QNIC_E, .index = 0, .address = 1}});
   req->setStack_of_QNICs(1, QNIC_id_pair{.fst = {.type = QNIC_E, .index = 0, .address = 2}, .snd = {.type = QNIC_E, .index = 1, .address = 3}});
   req->setStack_of_QNICs(1, QNIC_id_pair{.fst = {.type = QNIC_E, .index = 0, .address = 4}, .snd = {.type = QNIC_E, .index = 1, .address = 5}});
@@ -73,7 +90,69 @@ TEST(ConnectionManagerTest, RespondToRequest) {
   EXPECT_CALL(*routing_daemon, return_QNIC_address_to_destAddr(4)).Times(1).WillOnce(Return(5));
   auto src_info = new ConnectionSetupInfo{};
   EXPECT_CALL(*hardware_monitor, findConnectionInfoByQnicAddr(5)).Times(1).WillOnce(Return(ByMove(std::unique_ptr<ConnectionSetupInfo>(src_info))));
-  connection_manager.respondToRequest(req);
+
+  sim->setContext(connection_manager);
+  connection_manager->respondToRequest(req);
+  auto gate = connection_manager->toRouterGate;
+  EXPECT_EQ(gate->messages.size(), 4);
+
+  // checking the ruleset for QNode2(initiator)
+  {
+    auto *packetFor2 = dynamic_cast<ConnectionSetupResponse *>(gate->messages[0]);
+    ASSERT_NE(packetFor2, nullptr);
+    EXPECT_EQ(packetFor2->getDestAddr(), 2);
+    auto *ruleset = packetFor2->getRuleSet();
+    ASSERT_NE(ruleset, nullptr);
+    EXPECT_EQ(ruleset->size(), 4);
+
+    // checking the 1st rule
+    {
+      auto *rule = ruleset->rules.at(0).get();
+      EXPECT_EQ(rule->name, "X purification with : 3");
+      EXPECT_EQ(rule->condition->clauses.size(), 1);
+      EXPECT_NE(dynamic_cast<PurifyAction *>(rule->action.get()), nullptr);
+      EXPECT_NE(dynamic_cast<EnoughResourceClause *>(rule->condition.get()->clauses.at(0)), nullptr);
+    }
+
+    // checking the 2nd rule
+    {
+      auto *rule = ruleset->rules.at(1).get();
+      EXPECT_EQ(rule->name, "Wait rule with: 3 to ");
+      EXPECT_EQ(rule->condition.get(), nullptr);
+      EXPECT_EQ(rule->action.get(), nullptr);
+    }
+
+    // checking the 3rd rule
+    {
+      auto *rule = ruleset->rules.at(2).get();
+      EXPECT_EQ(rule->name, "X purification with : 5");
+      EXPECT_EQ(rule->condition->clauses.size(), 1);
+      EXPECT_NE(dynamic_cast<PurifyAction *>(rule->action.get()), nullptr);
+      EXPECT_NE(dynamic_cast<EnoughResourceClause *>(rule->condition.get()->clauses.at(0)), nullptr);
+    }
+
+    // checking the 4th rule
+    {
+      auto *rule = ruleset->rules.at(3).get();
+      EXPECT_EQ(rule->name, "tomography");
+      EXPECT_EQ(rule->condition->clauses.size(), 2);
+      EXPECT_NE(dynamic_cast<RandomMeasureAction *>(rule->action.get()), nullptr);
+      EXPECT_NE(dynamic_cast<MeasureCountClause *>(rule->condition.get()->clauses.at(0)), nullptr);
+      EXPECT_NE(dynamic_cast<EnoughResourceClause *>(rule->condition.get()->clauses.at(1)), nullptr);
+    }
+  }
+
+  auto *packetFor3 = dynamic_cast<ConnectionSetupResponse *>(gate->messages[1]);
+  ASSERT_NE(packetFor3, nullptr);
+  EXPECT_EQ(packetFor3->getDestAddr(), 3);
+
+  auto *packetFor4 = dynamic_cast<ConnectionSetupResponse *>(gate->messages[2]);
+  ASSERT_NE(packetFor4, nullptr);
+  EXPECT_EQ(packetFor4->getDestAddr(), 4);
+
+  auto *packetFor5 = dynamic_cast<ConnectionSetupResponse *>(gate->messages[3]);
+  ASSERT_NE(packetFor5, nullptr);
+  EXPECT_EQ(packetFor5->getDestAddr(), 5);
 }
 
 TEST(ConnectionManagerTest, GetQNICInterface) {
