d5ba323bf3655802d8eba48b5cc0af9a5dc1fcea
zigen <hrlclb@gmail.com>
2022-03-10 20:19:53 +0900

    move BackendContainer impl to cc file

--- quisp/modules/Backend/Backend.cc
+++ quisp/modules/Backend/Backend.cc
@@ -3,5 +3,22 @@
 namespace quisp::modules::backend {
 BackendContainer::BackendContainer() {}
 BackendContainer::~BackendContainer() {}
+void BackendContainer::initialize() {
+  auto backend_type = std::string(par("backendType").stringValue());
+  if (backend_type == "ErrorTrackingBackend") {
+    backend = std::make_unique<ErrorTrackingBackend>(std::make_unique<RNG>(this));
+  } else {
+    throw omnetpp::cRuntimeError("Unknown backend type: %s", backend_type.c_str());
+  }
+}
+
+void BackendContainer::finish() {}
+
+IQuantumBackend* BackendContainer::getQuantumBackend() {
+  if (backend == nullptr) {
+    throw omnetpp::cRuntimeError("Backend is not initialized");
+  }
+  return backend.get();
+}
 
 }  // namespace quisp::modules::backend
--- quisp/modules/Backend/Backend.h
+++ quisp/modules/Backend/Backend.h
@@ -14,23 +14,10 @@ class BackendContainer : public omnetpp::cSimpleModule {
   BackendContainer();
   ~BackendContainer();
 
-  void initialize() override {
-    auto backend_type = std::string(par("backend_type").stringValue());
-    if (backend_type == "ErrorTrackingBackend") {
-      backend = std::make_unique<ErrorTrackingBackend>(std::make_unique<RNG>(this));
-    } else {
-      throw omnetpp::cRuntimeError("Unknown backend type: %s", backend_type.c_str());
-    }
-  }
+  void initialize() override;
+  void finish() override;
 
-  void finish() override {}
-
-  IQuantumBackend* getQuantumBackend() {
-    if (backend == nullptr) {
-      throw omnetpp::cRuntimeError("Backend is not initialized");
-    }
-    return backend.get();
-  }
+  IQuantumBackend* getQuantumBackend();
 
  protected:
   std::unique_ptr<IQuantumBackend> backend;
