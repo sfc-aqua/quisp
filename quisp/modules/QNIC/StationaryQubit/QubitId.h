7eec57c0f16086b14153b80cefd34548b3130e06
zigen <hrlclb@gmail.com>
2022-03-22 15:48:40 +0900

    fix hash_combine in QubitId

--- quisp/modules/QNIC/StationaryQubit/QubitId.h
+++ quisp/modules/QNIC/StationaryQubit/QubitId.h
@@ -1,18 +1,17 @@
+#pragma once
 #include "backends/Backends.h"
 #include "modules/QNIC/StationaryQubit/IStationaryQubit.h"
 
 namespace quisp::modules::qubit_id {
-
-void hash_combine(std::size_t& seed, int const& v) { seed ^= std::hash<int>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2); }
 
 class QubitId : public quisp::backends::IQubitId {
  public:
   QubitId(int node_addr, int qnic_index, int qnic_type, int qubit_addr) : node_addr(node_addr), qnic_index(qnic_index), qnic_type(qnic_type), qubit_addr((qubit_addr)) {}
   std::size_t hash() const override {
     size_t seed = std::hash<int>()(node_addr);
-    hash_combine(seed, qnic_index);
-    hash_combine(seed, qnic_type);
-    hash_combine(seed, qubit_addr);
+    hashCombine(seed, qnic_index);
+    hashCombine(seed, qnic_type);
+    hashCombine(seed, qubit_addr);
     return seed;
   }
 
@@ -23,6 +22,7 @@ class QubitId : public quisp::backends::IQubitId {
 
  protected:
   // https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine
+  void hashCombine(std::size_t& seed, int const& v) const { seed ^= std::hash<int>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2); }
 
   int node_addr;
   int qnic_index;
