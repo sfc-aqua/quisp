de75d68c41a50269d49607c36bd0cefe7a25dc4e
zigen <hrlclb@gmail.com>
2022-06-16 12:34:48 +0900

    cleanup docker related shell scripts

--- docker_build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build --build-arg VERSION=6.0 -t quisp .
--- docker_run_beta.sh
+++ docker_run_beta.sh
@@ -9,5 +9,5 @@ else
     xterm -e "$(xhost $IP)"
 fi
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-docker run --privileged --rm -it -v "$(pwd):/root/quisp" --name quisp -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$IP:0" quisp
+docker run --privileged --rm -it -v "$(pwd):/root/quisp" -w /root/quisp --name quisp -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$IP:0" ghcr.io/sfc-aqua/quisp bash
 trap "lsof -i:6000 -t|xargs kill" 0
