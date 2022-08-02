771d11c2940a1fee55f382c60e11a11007bb3850
zigen <hrlclb@gmail.com>
2022-08-02 15:45:53 +0900

    add test logger

---
+++ quisp/test_utils/Logger.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <modules/Logger/ILogger.h>
+namespace quisp_test::Logger {
+using quisp::modules::Logger::ILogger;
+class TestLogger : public ILogger {
+  public:
+  TestLogger(){}
+  ~TestLogger(){}
+  void logPacket(std::string event_type, omnetpp::cMessage* msg) override {
+    std::cout << "logPacket: " << event_type << std::endl;
+  };
+  void setModule(omnetpp::cModule*mod) override{ }
+};
+}  // namespace quisp_test::Logger
