a2f41453f45b78d88d2e3f04cbd6a5d2621c9105
zigen <hrlclb@gmail.com>
2022-09-15 17:35:37 +0900

    temp

---
+++ quisp/runtime/Runtime.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <rules/Active/ActiveRuleSet.h>
+
+namespace quisp::runtime {
+class Runtime {
+ public:
+  Runtime() {}
+  ~Runtime() {}
+  void exec() {}
+};
+}  // namespace quisp::runtime
