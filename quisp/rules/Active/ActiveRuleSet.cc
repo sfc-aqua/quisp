bbaf09ce154cf58422dec4a8db727dcabe0c3edf
zigen <hrlclb@gmail.com>
2022-09-16 14:33:29 +0900

    temp: add basic structure for Runtime

--- quisp/rules/Active/ActiveRuleSet.cc
+++ quisp/rules/Active/ActiveRuleSet.cc
@@ -13,7 +13,9 @@ void ActiveRuleSet::addRule(std::unique_ptr<ActiveRule> r) { rules.emplace_back(
 std::unique_ptr<ActiveRule>& ActiveRuleSet::getRule(int i) { return rules[i]; };
 int ActiveRuleSet::size() const { return rules.size(); };
 bool ActiveRuleSet::empty() const { return rules.empty(); }
-std::vector<std::unique_ptr<ActiveRule>>::const_iterator ActiveRuleSet::cbegin() { return rules.cbegin(); }
-std::vector<std::unique_ptr<ActiveRule>>::const_iterator ActiveRuleSet::cend() { return rules.cend(); }
+std::vector<std::unique_ptr<ActiveRule>>::const_iterator ActiveRuleSet::cbegin() const noexcept { return rules.cbegin(); }
+std::vector<std::unique_ptr<ActiveRule>>::const_iterator ActiveRuleSet::cend() const noexcept { return rules.cend(); }
+std::vector<std::unique_ptr<ActiveRule>>::iterator ActiveRuleSet::begin() noexcept { return rules.begin(); }
+std::vector<std::unique_ptr<ActiveRule>>::iterator ActiveRuleSet::end() noexcept { return rules.end(); }
 
 }  // namespace quisp::rules::active
--- quisp/runtime/Runtime.h
+++ quisp/runtime/Runtime.h
@@ -1,12 +1,45 @@
 #pragma once
 
 #include <rules/Active/ActiveRuleSet.h>
+#include "rules/Active/ActiveAction.h"
+#include "rules/Active/ActiveCondition.h"
 
 namespace quisp::runtime {
 class Runtime {
  public:
   Runtime() {}
   ~Runtime() {}
-  void exec() {}
+  void exec(rules::active::ActiveRuleSet *ruleset) {
+    for (auto &rule : *ruleset) {
+      std::cout << "log: " << rule->name << std::endl;
+      auto &condition = rule->condition;
+      auto &action = rule->action;
+      if (!checkCondition(condition)) {
+        break;
+      }
+      execAction(action);
+    }
+  }
+
+  bool checkCondition(std::unique_ptr<rules::active::ActiveCondition> &condition) { return false; }
+
+  void execAction(std::unique_ptr<rules::active::ActiveAction> &action) {}
 };
+namespace op {
+// Resource Preparations
+void assign() {}
+void validate() {}
+// Quantum Operations
+void gate() {}
+void measure() {}
+
+// Messaging
+void send() {}
+
+// Post processing
+void freeQubit() {}
+void setNameToResource() {}
+void update() {}
+
+};  // namespace op
 }  // namespace quisp::runtime
