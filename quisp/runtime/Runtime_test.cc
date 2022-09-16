8bebd81aceb9803b94035b3290710627a0e0d8b4
zigen <hrlclb@gmail.com>
2022-09-16 14:33:29 +0900

    temp: add basic structure for Runtime

--- quisp/runtime/Runtime_test.cc
+++ quisp/runtime/Runtime_test.cc
@@ -1,7 +1,10 @@
 #include "Runtime.h"
 #include <gtest/gtest.h>
+#include "rules/Active/ActiveRuleSet.h"
 
 using namespace quisp::runtime;
+using namespace quisp::rules;
+using namespace quisp::rules::active;
 
 namespace {
 class RuntimeTest : public testing::Test {
@@ -15,4 +18,9 @@ class RuntimeTest : public testing::Test {
 };
 
 TEST_F(RuntimeTest, initialize) { ASSERT_NE(runtime, nullptr); }
+TEST_F(RuntimeTest, execSimpleRuleSet) {
+  auto rs = new ActiveRuleSet(0, 1);
+  rs->addRule(std::make_unique<ActiveRule>(0, 0, 3, "test"));
+  runtime->exec(rs);
+}
 }  // namespace
