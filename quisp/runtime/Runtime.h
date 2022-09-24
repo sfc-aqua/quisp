e8f8ad61f4d315792ebefe216d6195f74afa20ab
zigen <hrlclb@gmail.com>
2022-09-24 17:20:52 +0900

    implement branch oprations

--- quisp/runtime/Runtime.h
+++ quisp/runtime/Runtime.h
@@ -9,16 +9,18 @@
 #include "InstructionVisitor.h"
 #include "RuleSet.h"
 #include "macro_utils.h"
+#include "modules/QRSA/QRSA.h"
 #include "opcode.h"
 #include "types.h"
 
 namespace quisp::runtime {
 
-struct IQubitRecord;
+using IQubitRecord = quisp::modules::qrsa::IQubitRecord;
 
-class RuntimeError {
- public:
+struct RuntimeError {
+  RuntimeError(std::string msg, int pc) : message(msg), pc(pc) {}
   std::string message;
+  int pc;
 };
 
 struct Register {
@@ -30,9 +32,19 @@ struct Flags {
   bool test_result;
 };
 
+struct Hash {
+  size_t operator()(const std::pair<int, int>& p) const {
+    auto seed = std::hash<int>()(p.first);
+    // https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine
+    seed ^= std::hash<int>()(p.second) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    return seed;
+  }
+};
+
 // (partner's qnode addr, assigned RuleId) => [half bell pair qubit record]
-using QubitResources = std::unordered_multimap<std::pair<QNodeAddr, RuleId>, IQubitRecord*>;
+using QubitResources = std::unordered_multimap<std::pair<QNodeAddr, RuleId>, IQubitRecord*, Hash>;
 using QubitNameMap = std::unordered_map<QubitId, IQubitRecord*>;
+using LabelMap = std::unordered_map<Label, int>;
 
 class Runtime {
  public:
@@ -64,6 +76,8 @@ class Runtime {
   void eval(Program& program);
   void evalOperation(InstructionTypes op);
   void assignQubitToRuleSet(QNodeAddr partner_addr, IQubitRecord* qubit_record);
+  void collectLabels(Program& program);
+  void debugRuntimeState();
 
   // operations used by InstructionVisitor
   const Register& getReg(RegId regId) const;
@@ -71,6 +85,9 @@ class Runtime {
   void setRegVal(RegId regId, unsigned long long val);
   void setQubit(IQubitRecord*, QubitId);
   IQubitRecord* getQubitByPartnerAddr(QNodeAddr, int);
+  IQubitRecord* getQubitByQubitId(QubitId) const;
+  void jumpTo(const Label&);
+  void setError(const String& message);
 
   // related components
   InstructionVisitor visitor;
@@ -78,12 +95,13 @@ class Runtime {
 
   // states
   /// @brief current rule evaluated
-  RuleId rule_id;
+  RuleId rule_id = -1;
   /// @brief program counter for execution
   unsigned int pc = 0;
   Register registers[4];
   QubitResources qubits;
   QubitNameMap named_qubits;
   RuntimeError* error = nullptr;
+  LabelMap label_map;
 };
 }  // namespace quisp::runtime
--- quisp/runtime/Runtime_test.cc
+++ quisp/runtime/Runtime_test.cc
@@ -1,58 +1,107 @@
 #include "Runtime.h"
 #include <gtest/gtest.h>
+#include <modules/QRSA/RuleEngine/QubitRecord/QubitRecord.h>
+#include "modules/QRSA/QRSA.h"
 #include "rules/Active/ActiveRuleSet.h"
+#include "runtime/types.h"
 
 using namespace quisp::runtime;
 using namespace quisp::rules;
 using namespace quisp::rules::active;
-
+using quisp::modules::qubit_record::QubitRecord;
 namespace {
 class RuntimeTest : public testing::Test {
  protected:
-  void SetUp() { runtime = new Runtime(); }
+  void SetUp() {
+    runtime = new Runtime();
+    qubit = new QubitRecord{QNIC_E, 2, 3};
+  }
 
   void TearDown() { delete runtime; }
 
  public:
   Runtime* runtime;
+  qrsa::IQubitRecord* qubit;
 };
 
 TEST_F(RuntimeTest, initialize) { ASSERT_NE(runtime, nullptr); }
+
 TEST_F(RuntimeTest, execSimpleRuleSet) {
-  Rule rule{Program{"cond1",
-                    {
-                        {INSTR_NOP_int_(0)},
-                    }},
-            Program{"action1", {{INSTR_NOP_int_(0)}}}};
-  Program p{"cond2", {INSTR_DEBUG_String_{"test"}}};
-  std::cout << p.opcodes[0].index() << std::endl;
   RuleSet rs{"1st ruleset",
              {
                  Rule{Program{"cond2",
                               {
-                                  INSTR_DEBUG_int_{1},
-                                  INSTR_SET_RegId_int_{{RegId::REG0, 3}},
-                                  INSTR_ADD_RegId_RegId_RegId_{{RegId::REG0, RegId::REG0, RegId::REG0}},
-                                  INSTR_DEBUG_RegId_{RegId::REG0},
-                                  INSTR_ADD_RegId_RegId_int_{{RegId::REG0, RegId::REG0, 1}},
-                                  INSTR_DEBUG_RegId_{RegId::REG0},
+                                  // clang-format off
+INSTR_SET_RegId_int_{{RegId::REG0, 3}},
+INSTR_DEBUG_RegId_{RegId::REG0},
+INSTR_ADD_RegId_RegId_RegId_{{RegId::REG0, RegId::REG0, RegId::REG0}},
+INSTR_DEBUG_RegId_{RegId::REG0},
+INSTR_ADD_RegId_RegId_int_{{RegId::REG0, RegId::REG0, 1}},
+INSTR_DEBUG_RegId_{RegId::REG0},
+                                  // clang-format on
                               }},
-                      Program{"cond2", {INSTR_DEBUG_String_{"test"}}}},
+                      Program{"cond2", {}}},
 
              }};
+  rs.finalize();
   runtime->exec(std::move(rs));
-  // runtime->exec(RuleSet{"test ruleset"});
+}
 
-  // auto count = RegId::REG0;
-  // auto outcome = RegId::REG1;
-  // auto qubit0 = QubitId::Qubit0;
-  // Program{"RandomMeasureAction", {
-  //   INSTR_GET_QUBIT_QubitId_PartnerId_{Qubit0}
-  //   INSTR_BEQ_Label_{3},
-  //   INSTR_ERROR_String_{"Qubit not found for mesaurement"},
-  //   INSTR_MEASURE_RegId_QubitId_{count, Qubit0},
-  //   INSTR_INC_RegId_{RegId::REG1},
-  //   INSTR_SEND_LINK_TOMOGRAPHY_RESULT{RegId::REG0,RegId::REG1}
-  // }};
+TEST_F(RuntimeTest, evalQubitIdOperation) {
+  auto q0 = 0;
+  QNodeAddr partner_addr = 1;
+  auto qubit_index = 0;  // former 'resource'
+  runtime->assignQubitToRuleSet(2, qubit);
+  Program program{"RandomMeasureAction",
+                  {
+                      // clang-format off
+INSTR_GET_QUBIT_QubitId_QNodeAddr_int_{{q0, partner_addr, qubit_index}},
+INSTR_BNERR_Label_{"L1"},
+INSTR_ERROR_String_{"Qubit not found for mesaurement"},
+// INSTR_MEASURE_MemoryKey_QubitId_{{"output", Qubit0}, "L1"},
+INSTR_INC_RegId_{RegId::REG1},
+// INSTR_SEND_LINK_TOMOGRAPHY_RESULT{RegId::REG0,RegId::REG1}
+                      // clang-format on
+                  }};
+  runtime->eval(program);
+}
+
+TEST_F(RuntimeTest, jump) {
+  auto r0 = RegId::REG0;
+  Program program{"JumpTest",
+                  {
+                      // clang-format off
+INSTR_DEBUG_RUNTIME_STATE_None_{nullptr},
+INSTR_SET_RegId_int_{{r0, 10}},
+INSTR_JMP_Label_{"test"},
+INSTR_DEBUG_RUNTIME_STATE_None_{nullptr},
+INSTR_ADD_RegId_RegId_int_{{r0, r0, 1}},
+INSTR_DEBUG_RUNTIME_STATE_None_{nullptr},
+INSTR_DEBUG_RegId_{r0, "test"},
+                      // clang-format on
+                  }};
+  runtime->cleanup();
+  runtime->eval(program);
+  EXPECT_EQ(runtime->getRegVal(r0), 10);
+}
+
+TEST_F(RuntimeTest, branch_if_no_error) {
+  auto r0 = RegId::REG0;
+  Program program{"BNErrTest",
+                  {
+                      // clang-format off
+INSTR_DEBUG_RUNTIME_STATE_None_{nullptr},
+INSTR_SET_RegId_int_{{r0, 10}},
+INSTR_BNERR_Label_{"test"},
+// skip until "test" label
+INSTR_DEBUG_RUNTIME_STATE_None_{nullptr},
+INSTR_ADD_RegId_RegId_int_{{r0, r0, 3}},
+INSTR_DEBUG_RUNTIME_STATE_None_{nullptr},
+INSTR_DEBUG_RegId_{r0, "test"},
+                      // clang-format on
+                  }};
+  runtime->cleanup();
+  runtime->eval(program);
+  EXPECT_EQ(runtime->getRegVal(r0), 10);
 }
 }  // namespace
