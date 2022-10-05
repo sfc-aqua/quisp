b4cc08093e06880fe65325f1354f37af10698cf3
zigen <hrlclb@gmail.com>
2022-10-05 22:13:52 +0900

    [runtime]: collect partner addrs in RS finalization

--- quisp/runtime/RuleSet.cc
+++ quisp/runtime/RuleSet.cc
@@ -1,4 +1,6 @@
 #include "RuleSet.h"
+#include <variant>
+#include "runtime/types.h"
 
 namespace quisp::runtime {
 void Rule::finalize() {}
@@ -8,5 +10,23 @@ void RuleSet::finalize() {
   for (int i = 0; i < rules.size(); ++i) {
     rules[i].id = i;
   }
+
+  // collect partner addresses
+  partners = {};
+  for (auto &rule : rules) {
+    for (auto &instr : rule.condition.opcodes) {
+      if (std::holds_alternative<INSTR_GET_QUBIT_QubitId_QNodeAddr_int_>(instr)) {
+        auto [_qubit_id, partner_addr, _index] = std::get<INSTR_GET_QUBIT_QubitId_QNodeAddr_int_>(instr).args;
+        partners.insert(partner_addr);
+      }
+    }
+    for (auto &instr : rule.action.opcodes) {
+      if (std::holds_alternative<INSTR_GET_QUBIT_QubitId_QNodeAddr_int_>(instr)) {
+        auto [_qubit_id, partner_addr, _index] = std::get<INSTR_GET_QUBIT_QubitId_QNodeAddr_int_>(instr).args;
+        partners.insert(partner_addr);
+      }
+    }
+  }
+  finalized = true;
 }
 }  // namespace quisp::runtime
