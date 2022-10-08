bb4665572516aa2b54146c7ddf65da2145b3fb09
zigen <hrlclb@gmail.com>
2022-10-08 21:01:07 +0900

    [runtime]: add dedicated types for primitives

--- quisp/runtime/types.cc
+++ quisp/runtime/types.cc
@@ -1,5 +1,41 @@
 #include "types.h"
+#include <functional>
+#include <iostream>
+#include <string>
+
 namespace quisp::runtime {
+
+QNodeAddr::QNodeAddr(int val) : val(val) {}
+std::ostream& operator<<(std::ostream& stream, const QNodeAddr& value) {
+  stream << "QNodeAddr(" << std::to_string(value.val) << ")";
+  return stream;
+}
+bool operator<(const QNodeAddr& a, const QNodeAddr& b) { return a.val < b.val; }
+bool operator==(const QNodeAddr& a, const QNodeAddr& b) { return a.val == b.val; }
+
+QubitId::QubitId(int val) : val(val) {}
+size_t QubitId::operator()(const QubitId& id) const { return std::hash<int>()(id.val); }
+std::ostream& operator<<(std::ostream& stream, const QubitId& value) {
+  stream << "QubitId(" << std::to_string(value.val) << ")";
+  return stream;
+}
+bool operator<(const QubitId& a, const QubitId& b) { return a.val < b.val; }
+bool operator==(const QubitId& a, const QubitId& b) { return a.val == b.val; }
+
+Label::Label(std::string val) : val(val) {}
+std::ostream& operator<<(std::ostream& stream, const Label& label) {
+  stream << "Label(" << label.val << ")";
+  return stream;
+}
+bool operator==(const Label& a, const Label& b) { return a.val == b.val; }
+
+MemoryKey::MemoryKey(std::string key) : val(key) {}
+std::ostream& operator<<(std::ostream& stream, const MemoryKey& key) {
+  stream << "MemoryKey(" << key.val << ")";
+  return stream;
+}
+bool operator==(const MemoryKey& a, const MemoryKey& b) { return a.val == b.val; }
+
 std::ostream& operator<<(std::ostream& stream, const RegId& value) {
   switch (value) {
     case RegId::REG0:
