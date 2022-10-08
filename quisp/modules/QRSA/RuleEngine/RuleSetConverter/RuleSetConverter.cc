2d1de04bb138da2498892f5a2d33212b9fb97ea5
zigen <hrlclb@gmail.com>
2022-10-08 21:03:39 +0900

    [re]: use new runtime primitive types

--- quisp/modules/QRSA/RuleEngine/RuleSetConverter/RuleSetConverter.cc
+++ quisp/modules/QRSA/RuleEngine/RuleSetConverter/RuleSetConverter.cc
@@ -76,15 +76,15 @@ Program RuleSetConverter::constructTomographyAction(const Tomography *act) {
   return Program{"Tomography",
                  {
                      // clang-format off
-INSTR_LOAD_RegId_MemoryKey_{{count, "count"}},
+INSTR_LOAD_RegId_MemoryKey_{{count, MemoryKey{"count"}}},
 INSTR_GET_QUBIT_QubitId_QNodeAddr_int_{{q0, partner_addr, qubit_resource_index}},
-INSTR_BNERR_Label_{"L1"},
+INSTR_BNERR_Label_{Label{"L1"}},
 INSTR_ERROR_String_{"Qubit not found for mesaurement"},
-INSTR_MEASURE_RANDOM_MemoryKey_QubitId_{{"outcome", q0}, "L1"},
+INSTR_MEASURE_RANDOM_MemoryKey_QubitId_{{MemoryKey{"outcome"}, q0}, "L1"},
 INSTR_INC_RegId_{count},
-INSTR_STORE_MemoryKey_RegId_{{"count", count}},
+INSTR_STORE_MemoryKey_RegId_{{MemoryKey{"count"}, count}},
 INSTR_FREE_QUBIT_QubitId_{q0},
-INSTR_SEND_LINK_TOMOGRAPHY_RESULT_QNodeAddr_RegId_MemoryKey_int_{{partner_addr, count, "outcome", max_count}}
+INSTR_SEND_LINK_TOMOGRAPHY_RESULT_QNodeAddr_RegId_MemoryKey_int_{{partner_addr, count,MemoryKey{"outcome"}, max_count}}
                      // clang-format on
                  }};
 }
