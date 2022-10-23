688771f4a61aef90d8dc83d3618215d46db1419f
zigen <hrlclb@gmail.com>
2022-10-24 00:31:52 +0900

    [runtime]: cleanup debugging mechanism

--- quisp/runtime/RuleSet.h
+++ quisp/runtime/RuleSet.h
@@ -9,28 +9,31 @@
 namespace quisp::runtime {
 class Program {
  public:
-  Program(const std::string& name, const std::vector<InstructionTypes>& opcodes) : name(name), opcodes(opcodes) {}
+  Program(const std::string& name, const std::vector<InstructionTypes>& opcodes, bool debugging = false) : name(name), opcodes(opcodes), debugging(debugging) {}
 
   std::string name;
   std::vector<InstructionTypes> opcodes;
+  bool debugging = false;
 };
 
 class Rule {
  public:
-  Rule(const Program& condition, const Program& action) : Rule("", -1, condition, action) {}
-  Rule(const std::string& name, int shared_tag, const Program& condition, const Program& action) : name(name), shared_tag(shared_tag), condition(condition), action(action) {}
+  Rule(const Program& condition, const Program& action, bool debugging = false) : Rule("", -1, condition, action, debugging) {}
+  Rule(const std::string& name, int shared_tag, const Program& condition, const Program& action, bool debugging = false)
+      : name(name), shared_tag(shared_tag), condition(condition), action(action), debugging(debugging) {}
   void finalize();
   std::string name;
   int id = -1;
   int shared_tag = -1;
   Program condition;
   Program action;
+  bool debugging = false;
 };
 
 class RuleSet {
  public:
   RuleSet(const std::string& name = "") : name(name), rules(std::vector<Rule>()) {}
-  RuleSet(const std::string& name, const std::vector<Rule>& rules) : name(name), rules(rules) {}
+  RuleSet(const std::string& name, const std::vector<Rule>& rules, bool debugging = false) : name(name), rules(rules), debugging(debugging) {}
   void finalize();
 
   // @brief partners contains partner QNodeAddrs
@@ -45,6 +48,7 @@ class RuleSet {
   int owner_addr;
   std::string name;
   std::vector<Rule> rules;
+  bool debugging = false;
 
  protected:
   static inline void collectPartners(const RuleId rule_id, const InstructionTypes& instr, std::set<QNodeAddr>& partners,
