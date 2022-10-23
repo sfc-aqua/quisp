1da9143ae4db77929ffe7ea3ec485c8c17d32073
zigen <hrlclb@gmail.com>
2022-10-23 18:58:27 +0900

    [re]: allow purification result table to show as string for debugging

---
+++ quisp/modules/QRSA/RuleEngine/PurificationResultTable/PurificationResultTable.cc
@@ -0,0 +1,11 @@
+#include "PurificationResultTable.h"
+
+namespace std {
+
+std::ostream &operator<<(std::ostream &os, const quisp::modules::pur_result_table::PurificationResultTable &table) {
+  for (auto &[key, data] : table) {
+      os << "(rule_id: " << key.rule_id<< ", tag:" << key.shared_tag<< ", act" << key.action_index << "): is_x_plus:" << data.is_x_plus << ", ";
+  }
+  return os;
+}
+}
