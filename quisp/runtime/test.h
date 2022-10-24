59a327e3f4aa449369a580e295b118d1d546d5f0
zigen <hrlclb@gmail.com>
2022-10-24 09:28:55 +0900

    [runtime]: fix unit tests

--- quisp/runtime/test.h
+++ quisp/runtime/test.h
@@ -12,6 +12,8 @@ using quisp::runtime::QNodeAddr;
 class MockRuntimeCallback : public quisp::runtime::Runtime::ICallBack {
  public:
   MOCK_METHOD(MeasurementOutcome, measureQubitRandomly, (IQubitRecord*), (override));
+  MOCK_METHOD(MeasurementOutcome, measureQubitX, (IQubitRecord*), (override));
+  MOCK_METHOD(MeasurementOutcome, measureQubitZ, (IQubitRecord*), (override));
   MOCK_METHOD(void, gateX, (IQubitRecord*), (override));
   MOCK_METHOD(void, gateZ, (IQubitRecord*), (override));
   MOCK_METHOD(void, gateCNOT, (IQubitRecord*, IQubitRecord*), (override));
@@ -29,5 +31,6 @@ class MockRuntimeCallback : public quisp::runtime::Runtime::ICallBack {
   MOCK_METHOD(void, freeAndResetQubit, (IQubitRecord*), (override));
   MOCK_METHOD(bool, isQubitLocked, (IQubitRecord* const), (override));
   MOCK_METHOD(void, lockQubit, (IQubitRecord* const, unsigned long rs_id, int rule_id, int action_index), (override));
+  MOCK_METHOD(void, hackSwappingPartners, (IQubitRecord* const, IQubitRecord* const), (override));
 };
 }  // namespace quisp_test
