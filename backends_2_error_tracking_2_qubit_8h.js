d1afb66bb8362d5ae44560a3c85b0f2c79b9a28d
MakotoNakai <MakotoNakai@users.noreply.github.com>
2022-10-25 10:35:18 +0000

    deploy: 733e45be0ce9edf10d8ca27cd01d5b40dba1f2c0

--- backends_2_error_tracking_2_qubit_8h.js
+++ backends_2_error_tracking_2_qubit_8h.js
@@ -1,4 +1,4 @@
-var backends_2_error_tracking_2_q_u_b_i_t_8h =
+var backends_2_error_tracking_2_qubit_8h =
 [
     [ "quisp::backends::error_tracking::ErrorTrackingQubit", "classquisp_1_1backends_1_1error__tracking_1_1_error_tracking_qubit.html", "classquisp_1_1backends_1_1error__tracking_1_1_error_tracking_qubit" ]
 ];
