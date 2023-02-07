0169dac6c47484b9040d59465e9aff3e0ca96aa8
zigen <hrlclb@gmail.com>
2023-02-07 06:49:47 +0000

    add NoError MIM network test

--- simulation_tests/simulation_test.py
+++ simulation_tests/simulation_test.py
@@ -1,31 +1,24 @@
 from .utils import Worker, parse_output
 import pytest
 
+
 @pytest.mark.asyncio
 async def test_NoErrorMIM():
     worker = Worker()
-    await worker.run(config_name="NoErrorMIM", ned_file_path="simulations/simulation_test.ini")
-    worker.print_results()
+    await worker.run(
+        config_name="NoErrorMIM", ned_file_path="simulations/simulation_test.ini"
+    )
     print(worker.output)
-
-
-@pytest.mark.asyncio
-async def test_0():
-    worker = Worker()
-    await worker.run("Test0")
     worker.print_results()
-
-    # FIXME: the results seem wrong. we put the temporary results to check the simulation tests work.
-    assert worker.results["EndNode2[0]<-->Repeater1[0]"]["data"], {
-        "Fidelity": 0.875412,
-        "Xerror": 0.00346101,
-        "Zerror": 0.124588,
-        "Yerror": -0.00346101,
+    assert worker.results["EndNode1<-->EndNode2"]["data"] == {
+        "Fidelity": 1.0,
+        "Xerror": 0.0,
+        "Zerror": 0.0,
+        "Yerror": 0.0,
     }
-    assert worker.results["Repeater1[0]<-->EndNode2[0]"]["data"], {
-        "Fidelity": 0.875412,
-        "Xerror": 0.00346101,
-        "Zerror": 0.124588,
-        "Yerror": -0.00346101,
+    assert worker.results["EndNode2<-->EndNode1"]["data"] == {
+        "Fidelity": 1.0,
+        "Xerror": 0.0,
+        "Zerror": 0.0,
+        "Yerror": 0.0,
     }
-
