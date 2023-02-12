eae554ce55455ed25638bdec1cf2eeb6c0b6de70
whit3z <whit3z@sfc.wide.ad.jp>
2023-02-12 22:26:08 +0900

    update simulation tests

--- simulation_tests/test_simple_error_case.py
+++ simulation_tests/test_simple_error_case.py
@@ -12,16 +12,16 @@ async def test_ChannelXErrorSimpleMIM():
     print(worker.output)
     worker.print_results()
     assert worker.results["EndNode1<-->EndNode2"]["data"] == {
-        "Fidelity": 0.495157,
-        "Xerror": 0.504843,
-        "Zerror": -0.0154454,
-        "Yerror": 0.0154454,
+        "Fidelity": 0.817657,
+        "Xerror": 0.182343,
+        "Zerror": -0.00335848,
+        "Yerror": 0.00335848,
     }
     assert worker.results["EndNode2<-->EndNode1"]["data"] == {
-        "Fidelity": 0.495157,
-        "Xerror": 0.504843,
-        "Zerror": -0.0154454,
-        "Yerror": 0.0154454,
+        "Fidelity": 0.817657,
+        "Xerror": 0.182343,
+        "Zerror": -0.00335848,
+        "Yerror": 0.00335848,
     }
 
 
@@ -35,14 +35,14 @@ async def test_ChannelXErrorSimpleMM():
     print(worker.output)
     worker.print_results()
     assert worker.results["EndNode1<-->EndNode2"]["data"] == {
-        "Fidelity": 0.503945,
-        "Xerror": 0.496055,
-        "Zerror": 0.00766231,
-        "Yerror": -0.00766231,
+        "Fidelity": 0.905137,
+        "Xerror": 0.0948629,
+        "Zerror": -0.00540325,
+        "Yerror": 0.00540325,
     }
     assert worker.results["EndNode2<-->EndNode1"]["data"] == {
-        "Fidelity": 0.503945,
-        "Xerror": 0.496055,
-        "Zerror": 0.00766231,
-        "Yerror": -0.00766231,
+        "Fidelity": 0.905137,
+        "Xerror": 0.0948629,
+        "Zerror": -0.00540325,
+        "Yerror": 0.00540325,
     }
