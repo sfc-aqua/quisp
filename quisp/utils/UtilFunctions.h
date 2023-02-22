b600782a93abc93c44d40159a62d55bb14c2b9c5
Nozomi Tanetani <n.tanetani@gmail.com>
2023-02-22 18:21:54 +0900

    rand num inputs as an argument

--- quisp/utils/UtilFunctions.h
+++ quisp/utils/UtilFunctions.h
@@ -6,10 +6,7 @@ namespace quisp::utilFunctions {
 using quisp::backends::abstract::IRandomNumberGenerator;
 
 template <typename Label>
-Label sample(std::map<double, Label> weights) {
-  // Gives a random double between 0.0 ~ 1.0
-  std::unique_ptr<IRandomNumberGenerator> rng;
-  double rand = rng->doubleRandom();
+Label sample(std::map<double, Label> weights, double rand) {
   Label label;
 
   for (auto &[w, l] : weights) {
