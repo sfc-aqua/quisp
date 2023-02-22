f7906b35affb97743fc128798a38e2030b60814e
Nozomi Tanetani <n.tanetani@gmail.com>
2023-02-22 18:48:06 +0900

    add comments for new function

--- quisp/utils/UtilFunctions.h
+++ quisp/utils/UtilFunctions.h
@@ -5,6 +5,20 @@
 namespace quisp::utilFunctions {
 using quisp::backends::abstract::IRandomNumberGenerator;
 
+/*
+  Description:
+    This is a function that, given a set of labels and weights and a random number, returns a label according to the random number
+  Args:
+    std::map<double, Label> weights: weight(double) and label are a pair of values and it is a elements of std:map.
+    rand: random number(double)
+  Return:
+    It returns label which previous_label_weight < rand <= label_weight
+  Example:
+    weight = {{0.3, ErrorLabel::X}, {0.7, ErrorLabel::Z}, {1.0, ErrorLabel::Y}}}
+    rand = 0.4
+
+    returns ErrorLabel::Z
+ */
 template <typename Label>
 Label getLabel(std::map<double, Label> weights, double rand) {
   Label label;
