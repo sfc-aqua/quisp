ea993f660c5f68dea233e2914b19c1189fb0dd47
whit3z <whit3z@sfc.wide.ad.jp>
2023-02-28 02:12:50 +0900

    add purification simulation tests

--- simulation_tests/test_purification_single_x.py
+++ simulation_tests/test_purification_single_x.py
@@ -3,92 +3,92 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_ChannelXErrorSimpleMIM():
+async def test_Single_X_Purification_MIM_No_Error():
     worker = Worker()
     await worker.run(
-        config_name="ChannelXErrorSimpleMIM",
+        config_name="Single_X_Purification_MIM_No_Error",
         ned_file_path="simulations/simulation_test.ini",
     )
     print(worker.output)
     worker.print_results()
     assert worker.results["EndNode1<-->EndNode2"]["data"] == {
-        "Fidelity": 0.896712,
-        "Xerror": 0.103288,
-        "Zerror": -0.0103481,
-        "Yerror": 0.0103481,
+        "Fidelity": 1.0,
+        "Xerror": 0.0,
+        "Zerror": 0.0,
+        "Yerror": 0.0,
     }
     assert worker.results["EndNode2<-->EndNode1"]["data"] == {
-        "Fidelity": 0.896712,
-        "Xerror": 0.103288,
-        "Zerror": -0.0103481,
-        "Yerror": 0.0103481,
+        "Fidelity": 1.0,
+        "Xerror": 0.0,
+        "Zerror": 0.0,
+        "Yerror": 0.0,
     }
 
 
 @pytest.mark.asyncio
-async def test_ChannelXErrorSimpleMM():
+async def test_Single_X_Purification_MM_No_Error():
     worker = Worker()
     await worker.run(
-        config_name="ChannelXErrorSimpleMM",
+        config_name="Single_X_Purification_MM_No_Error",
         ned_file_path="simulations/simulation_test.ini",
     )
     print(worker.output)
     worker.print_results()
     assert worker.results["EndNode1<-->EndNode2"]["data"] == {
-        "Fidelity": 0.905137,
-        "Xerror": 0.0948629,
-        "Zerror": -0.00540325,
-        "Yerror": 0.00540325,
+        "Fidelity": 1.0,
+        "Xerror": 0.0,
+        "Zerror": 0.0,
+        "Yerror": 0.0,
     }
     assert worker.results["EndNode2<-->EndNode1"]["data"] == {
-        "Fidelity": 0.905137,
-        "Xerror": 0.0948629,
-        "Zerror": -0.00540325,
-        "Yerror": 0.00540325,
+        "Fidelity": 1.0,
+        "Xerror": 0.0,
+        "Zerror": 0.0,
+        "Yerror": 0.0,
     }
 
 
 @pytest.mark.asyncio
-async def test_MIM_Werner_State_Channel():
+async def test_Single_X_Purification_MIM_Werner_State():
     worker = Worker()
     await worker.run(
-        config_name="Channel_Error_Werner_State_MIM",
+        config_name="Single_X_Purification_MIM_Werner_State",
         ned_file_path="simulations/simulation_test.ini",
     )
     print(worker.output)
     worker.print_results()
     assert worker.results["EndNode1<-->EndNode2"]["data"] == {
-        "Fidelity": 0.694766,
-        "Xerror": 0.103959,
-        "Zerror": 0.100688,
-        "Yerror": 0.100587,
+        "Fidelity": 0.735104,
+        "Xerror": 0.0311294,
+        "Zerror": 0.197981,
+        "Yerror": 0.0357851,
     }
     assert worker.results["EndNode2<-->EndNode1"]["data"] == {
-        "Fidelity": 0.694766,
-        "Xerror": 0.103959,
-        "Zerror": 0.100688,
-        "Yerror": 0.100587,
+        "Fidelity": 0.735104,
+        "Xerror": 0.0311294,
+        "Zerror": 0.197981,
+        "Yerror": 0.0357851,
     }
 
 
 @pytest.mark.asyncio
-async def test_MM_Werner_State_Channel():
+async def test_Single_X_Purification_MM_Werner_State():
     worker = Worker()
     await worker.run(
-        config_name="Channel_Error_Werner_State_MM",
+        config_name="Single_X_Purification_MM_Werner_State",
         ned_file_path="simulations/simulation_test.ini",
     )
     print(worker.output)
     worker.print_results()
     assert worker.results["EndNode1<-->EndNode2"]["data"] == {
-        "Fidelity": 0.700108,
-        "Xerror": 0.104376,
-        "Zerror": 0.0904865,
-        "Yerror": 0.105029,
+        "Fidelity": 0.747207,
+        "Xerror": 0.0251512,
+        "Zerror": 0.19216,
+        "Yerror": 0.0354823,
     }
     assert worker.results["EndNode2<-->EndNode1"]["data"] == {
-        "Fidelity": 0.700108,
-        "Xerror": 0.104376,
-        "Zerror": 0.0904865,
-        "Yerror": 0.105029,
+        "Fidelity": 0.747207,
+        "Xerror": 0.0251512,
+        "Zerror": 0.19216,
+        "Yerror": 0.0354823,
     }
