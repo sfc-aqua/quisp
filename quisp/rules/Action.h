3767ad608dac902ea95f257b66242fb3e93c3217
soon <kento.soon@gmail.com>
2023-02-28 07:13:35 +0000

    implement steane purification

--- quisp/rules/Action.h
+++ quisp/rules/Action.h
@@ -25,6 +25,7 @@ enum PurType : int {
   DSDA_INV,  ///< Inverse Double Selection and Dual Action for both X and Z errors
   DSDA_SECOND,  ///< Different type of Double Selection and Dual Action for both X and Z errors
   DSDA_SECOND_INV,  ///< Different type of Inverse Double Selection and Dual Action for both X and Z errors
+  STEANE_DECODER, ///< Based on the decoding circuit of the [[7, 1, 3]] Steane code
 };
 
 NLOHMANN_JSON_SERIALIZE_ENUM(PurType, {
@@ -40,6 +41,7 @@ NLOHMANN_JSON_SERIALIZE_ENUM(PurType, {
                                           {DSDA_INV, "DSDA_INV"},
                                           {DSDA_SECOND, "DSDA_SECOND"},
                                           {DSDA_SECOND_INV, "DSDA_SECOND_INV"},
+                                          {STEANE_DECODER, "STEANE_DECODER"},
                                       })
 
 inline void to_json(json& j, const QnicInterface& qi) { j = json{{"partner_address", qi.partner_addr}}; }
