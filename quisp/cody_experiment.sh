e767d99ea600a8f8bc44bff277ede88ade96a256
soon <kento.soon@gmail.com>
2023-03-06 04:47:08 +0000

    add shell script for experiment

---
+++ quisp/cody_experiment.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_1km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_5km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_10km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_15km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_20km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_25km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_30km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MM_35km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_1km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_5km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_10km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_15km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_20km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_25km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_30km --cmdenv-express-mode=true
+./quisp simulations/cody_experiment.ini -u Cmdenv  -c MIM_35km --cmdenv-express-mode=true
