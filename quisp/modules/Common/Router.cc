b1dd48f18c54858580ebf582984c8ab9fe176c41
zigen <hrlclb@gmail.com>
2023-03-08 02:03:59 +0000

    temp

--- quisp/modules/Common/Router.cc
+++ quisp/modules/Common/Router.cc
@@ -48,13 +48,59 @@ void Router::initialize() {
     }
   }
 
-  auto *node = topo->getNodeFor(provider.getNode());
-  auto num_links = node->getNumOutLinks();
-  for (int i = 0; i < num_links; i++) {
-    auto *link = node->getLinkOut(i);
-    auto *mod = link->getRemoteNode()->getModule();
-    std::cout << mod ->getFullName() << std::endl;
+  // auto *node = topo->getNodeFor(provider.getNode());
+  // auto num_links = node->getNumOutLinks();
+  // for (int i = 0; i < num_links; i++) {
+  //   auto *link = node->getLinkOut(i);
+  //   auto *mod= node->getLinkOut(i)->getRemoteGate()->getNextGate()->getOwnerModule();
+  //   // auto *mod = link->getRemoteNode()->getModule();
+
+  //   std::cout << mod ->getFullName() << std::endl;
+  // }
+
+  {
+    auto *node = provider.getNode()->getSubmodule("qrsa");
+    if (node != nullptr) {
+      auto available_addr_list = provider.getAvailableAddresses();
+      int number_of_qnics = node->par("number_of_qnics");
+      int number_of_qnics_r = node->par("number_of_qnics_r");
+      int number_of_qnics_rp = node->par("number_of_qnics_rp");
+      for (int i = 0; i < number_of_qnics; i++) {
+        auto *qnic = provider.getQNIC(i, QNIC_E);
+        auto addr_str = qnic->gate("qnic_quantum_port$o")->getNextGate()->getNextGate()->getOwnerModule()->par("address").stringValue();
+        auto n_addr = QNodeAddr{addr_str};
+        bool in_network = false;
+        if (my_address.network_addr != n_addr.network_addr) {
+          for (auto &a : available_addr_list) {
+            if (a.network_addr == n_addr.network_addr) {
+              std::cout << my_address << ":" << a << ": " << qnic->getFullPath() << " : gate index:" << qnic->getIndex() << " addr:" << addr_str << std::endl;
+              in_network = true;
+              gate_network_map[qnic->getIndex()] = n_addr.network_addr;
+              break;
+            }
+          }
+        }
+        if (!in_network) {
+          gate_network_map[qnic->getIndex()] = my_address.network_addr;
+          std::cout << my_address << ": " << qnic->getFullPath() << " : " << addr_str << std::endl;
+        }
+      }
+      for (int i = 0; i < number_of_qnics_r; i++) {
+        auto *qnic = provider.getQNIC(i, QNIC_R);
+        auto addr_str = qnic->gate("qnic_quantum_port$o")->getNextGate()->getNextGate()->getOwnerModule()->par("address").stringValue();
+        std::cout << my_address << ": " << qnic->getFullPath() << " : " << addr_str << std::endl;
+      }
+    }
+
+    std::cout << "----" << std::endl;
+    for (auto [gate_i, network_addr] : gate_network_map) {
+      std::cout << my_address << ": gate_index: " << gate_i << ", network:" << network_addr << std::endl;
+    }
+    std::cout << "----" << std::endl;
   }
+  // auto * node = provider.getNode();
+
+  // node->gate("qnic_quantum_port$o")
 
   // Traverse through all the destinations from the thisNode
   for (int i = 0; i < topo->getNumNodes(); i++) {
@@ -119,6 +165,10 @@ void Router::handleMessage(cMessage *msg) {
   }
 
   int out_gate_index = (*it).second;
+  auto out_network = gate_network_map.find(out_gate_index);
+  if (out_network != gate_network_map.end()) {
+    // if the packet is going to another network
+  }
   pk->setHopCount(pk->getHopCount() + 1);
   send(pk, "toQueue", out_gate_index);
 }
