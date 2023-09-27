05299016acb2c51997625c779aafc06d0223ef1c
makotonakai <macofandohr1812@gmail.com>
2023-09-28 00:09:31 +0900

    [feat] add logger for InternalRuleSetForwarding_Application

--- quisp/modules/Logger/JsonLogger.cc
+++ quisp/modules/Logger/JsonLogger.cc
@@ -94,6 +94,16 @@ std::string JsonLogger::format(omnetpp::cMessage const* const msg) {
     return os.str();
   }
 
+  if (auto* req = dynamic_cast<const quisp::messages::InternalRuleSetForwarding_Application*>(msg)) {
+    std::stringstream os;
+    os << "\"msg_type\": \"InternalRuleSetForwarding_Application\"";
+    os << ", \"rule_id\": " << req->getRule_id();
+    os << ", \"ruleset_id\": " << req->getRuleSet_id();
+    os << ", \"ruleset\": " << req->getRuleSet();
+    os << ", \"application_type\": " << req->getApplication_type();
+    return os.str();
+  }
+
   return "\"msg\": \"unknown class\": \"" + msg->getFullPath() + "\"";
 }
 
