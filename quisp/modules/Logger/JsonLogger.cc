01f731f6cc5363fa267e09be061d485624276a1c
makotonakai <macofandohr1812@gmail.com>
2023-09-28 00:29:51 +0900

    [chore] update msg name

--- quisp/modules/Logger/JsonLogger.cc
+++ quisp/modules/Logger/JsonLogger.cc
@@ -106,7 +106,7 @@ std::string JsonLogger::format(omnetpp::cMessage const* const msg) {
 
   if (auto* req = dynamic_cast<const quisp::messages::InternalRuleSetForwarding*>(msg)) {
     std::stringstream os;
-    os << "\"msg_type\": \"InternalRuleSetForwarding_Application\"";
+    os << "\"msg_type\": \"InternalRuleSetForwarding\"";
     os << ", \"rule_id\": " << req->getRule_id();
     os << ", \"ruleset_id\": " << req->getRuleSet_id();
     os << ", \"ruleset\": " << req->getRuleSet();
