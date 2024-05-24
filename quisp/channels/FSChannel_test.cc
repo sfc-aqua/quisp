2cb4db3490a4a028040777ea357516c2cc168e8e
Paolo Fittipaldi <paolo.fittipaldi@lip6.fr>
2024-05-24 13:15:09 +0000

    Unit tests for FSChannel

--- quisp/channels/FSChannel_test.cc
+++ quisp/channels/FSChannel_test.cc
@@ -14,6 +14,7 @@ using namespace quisp_test::utils;
 using namespace quisp::messages;
 using namespace quisp::modules::SharedResource;
 using namespace omnetpp;
+using namespace quisp_test::utils;
 using OriginalFSChannel = quisp::channels::FSChannel;
 using quisp_test::channel_type::TestChannelType;
 
@@ -30,65 +31,32 @@ class MockNode : public quisp_test::TestQNode {
 class FSChannel : public OriginalFSChannel {
   public:
     FSChannel() : OriginalFSChannel::FSChannel() {
-      quisp_test::utils::setParDouble(this,"delay",0);
-      quisp_test::utils::setParDouble(this,"datarate",0);
-      quisp_test::utils::setParDouble(this,"ber",0);
-      quisp_test::utils::setParDouble(this,"per",0);
-      quisp_test::utils::setParBool(this,"disabled",false);
+      setParDouble(this,"distance",0);
+      setParDouble(this,"delay",0);
+      setParDouble(this,"datarate",0);
+      setParDouble(this,"ber",0);
+      setParDouble(this,"per",0);
+      setParDouble(this,"orbital_period",86400);
+      setParDouble(this,"speed_of_light_in_FS",299792458);
+      setParBool(this,"disabled",false);
+      setParBool(this,"CSV_varies_delay",true);
 
       setComponentType(new TestChannelType("test channel"));
     }
+    cChannel::Result public_processMessage(cMessage *msg) {return processMessage(msg, SendOptions(), simTime());
+    };
     void addResultRecorders() override {};
 };
 
-// class Strategy : public quisp_test::TestComponentProviderStrategy {
-//  public:
-//   Strategy(MockNode* _qnode) : parent_qnode(_qnode) {}
-//   cModule* getNode() override { return parent_qnode; }
-//   int getNodeAddr() override { return parent_qnode->address; }
-//   SharedResource* getSharedResource() override { return &shared_resource; }
-//  private:
-//   MockNode* parent_qnode;
-//   SharedResource shared_resource;
-// };
-// class FSChannel : public OriginalFSChannel {
-//  public:
-//   using OriginalFSChannel::handleMessage;
-//   using OriginalFSChannel::initialize;
-//   void addResultRecorders() override{};
-//   MockNode* parent;
-//   cModule* getParentModule() const override { return parent; };
-//   explicit FSChannel(MockNode* parent_qnode) : OriginalFSChannel() {
-//     this->provider.setStrategy(std::make_unique<Strategy>(parent_qnode));
-//     this->setComponentType(new TestModuleType("test_pointing_system"));
-//     parent = parent_qnode;
-//     auto* sim = getTestSimulation();
-//     sim->registerComponent(this);
-//     recPort = new TestGate(this, "rec");
-//     ansPort = new TestGate(this, "ans");
-//     this->addGate("rec", cGate::INPUT);
-//     this->addGate("ans", cGate::OUTPUT);
-//   }
-//   TestGate* ansPort;
-//   TestGate* recPort;
-//   std::map<const char*, cGate*> ports{};
-//   TestGate* gate(const char* gatename, int index = -1) override {
-//     if (strcmp(gatename, "rec") == 0) return recPort;
-//     if (strcmp(gatename, "ans") == 0) return ansPort;
-//     error("port: %s not found", gatename);
-//     return nullptr;
-//   }
-//   bool parentModuleIsQNode() { return dynamic_cast<MockNode*>(provider.getNode())->is_qnode; }
-//   void setIsQnode(bool is_qnode) { dynamic_cast<MockNode*>(provider.getNode())->is_qnode = is_qnode; }
-// };
-
 class TestSimpleModule : public cSimpleModule {
  public:
-  virtual void send(omnetpp::cMessage* msg, const char* gatename, int gateindex = -1) override {
+  virtual void scheduleAfter(simtime_t delay, cMessage* msg) override {
     take(msg);
-    cSimpleModule::send(msg, gatename, gateindex);
-  };
+    cSimpleModule::scheduleAfter(delay,msg);
+  }
+  virtual void addResultRecorders() override {};
 
+  virtual void handleMessage(cMessage*msg) override {};
   MockNode* parent;
   cModule* getParentModule() const override { return parent; };
 
@@ -99,9 +67,7 @@ class TestSimpleModule : public cSimpleModule {
     sim->registerComponent(this);
     port$o = new TestGate(this, "port$o");
     port$i = new TestGate(this, "port$i");
-
     this->addGate("port", cGate::INOUT);
-
   }
 
   TestGate* port$i;
@@ -111,7 +77,6 @@ class TestSimpleModule : public cSimpleModule {
   TestGate* gate(const char* gatename, int index = -1) override {
     if (strcmp(gatename, "port$i") == 0) return port$i;
     if (strcmp(gatename, "port$o") == 0) return port$o;
-
     error("port: %s not found", gatename);
     return nullptr;
   }
@@ -130,35 +95,22 @@ class FSChannelTest : public ::testing::Test {
     ground_node = new MockNode(2,0, false);
 
     sat_simplemodule = new TestSimpleModule(sat_node);
-    ground_simplemodule = new TestSimpleModule(ground_node);
 
     sat_gate = sat_node->addGate("sat_gate", cGate::INOUT);
     ground_gate = ground_node->addGate("ground_gate", cGate::INOUT);
 
-
     downlink_chl = new FSChannel();
-    uplink_chl = new FSChannel();
 
     sim->registerComponent(downlink_chl);
-    sim->registerComponent(uplink_chl);
-
 
     utils::setParStr(downlink_chl,"distance_CSV","channels/test_dist_csv.csv");
-    utils::setParStr(uplink_chl,"distance_CSV","channels/test_dist_csv.csv");
 
-    sat_simplemodule->gate("port$o")->quiet_connectTo(sat_node->gate("sat_gate$o"));
     sat_node->gate("sat_gate$o")->connectTo(ground_node->gate("ground_gate$i"), downlink_chl, true);
-    ground_node->gate("ground_gate$i")->connectTo(ground_simplemodule->gate("port$i"));
 
-    ground_simplemodule->gate("port$o")->quiet_connectTo(ground_node->gate("ground_gate$o"));
-    ground_node->gate("ground_gate$o")->connectTo(sat_node->gate("sat_gate$i"), uplink_chl, true);
-    sat_node->gate("sat_gate$i")->connectTo(sat_simplemodule->gate("port$i"));
+    sat_simplemodule->callInitialize();
 
     downlink_chl->finalizeParameters();  // THIS METHOD MAY ONLY BE CALLED WHEN THE CHANNEL IS CONNECTED
     downlink_chl->callInitialize();
-
-    uplink_chl->finalizeParameters();
-    uplink_chl->callInitialize();
   }
   void TearDown() {
     std::remove("channels/test_dist_csv.csv");
@@ -182,22 +134,61 @@ class FSChannelTest : public ::testing::Test {
 
   TestSimulation* sim;
   TestSimpleModule* sat_simplemodule;
-  TestSimpleModule* ground_simplemodule;
   MockNode* sat_node;
   MockNode* ground_node;
   FSChannel* downlink_chl;
-  FSChannel* uplink_chl;
-  cGate* sat_gate;
+ cGate* sat_gate;
   cGate* ground_gate;
 
 };
 
 TEST_F(FSChannelTest, messageWhenNonVisible) {
-  auto msg = new cMessage;
-  sat_simplemodule->send(msg,"sat_gate");
-  sim->executeNextEvent();
-  ASSERT_EQ(ground_simplemodule->gate("port")->messages.size(), 0);
+  cMessage *msg = new cMessage();
+  cChannel::Result res = downlink_chl->public_processMessage(msg);
+  sim->run();
+  ASSERT_EQ(res.discard, true);
 }
 
+TEST_F(FSChannelTest, messageWhenVisible) {
+  auto timeout = new cMessage;
+  sat_simplemodule->scheduleAfter(200,timeout);
+  sim->executeNextEvent();
+  auto msg = new cMessage;
+  cChannel::Result res = downlink_chl->public_processMessage(msg);
+  sim->run();
+  ASSERT_EQ(res.discard, false);
+  simtime_t delay = downlink_chl->par("distance").doubleValue()/downlink_chl->par("speed_of_light_in_FS").doubleValue();
+  ASSERT_EQ(res.delay.raw(),delay.raw());
+}
 
+TEST_F(FSChannelTest, messageAfterVisible) {
+  auto timeout = new cMessage;
+  sat_simplemodule->scheduleAfter(600,timeout);
+  sim->executeNextEvent();
+  auto msg = new cMessage;
+  cChannel::Result res = downlink_chl->public_processMessage(msg);
+  sim->run();
+  ASSERT_EQ(res.discard, true);
+}
+
+TEST_F(FSChannelTest, messageFollowingDayVisible) {
+  auto timeout = new cMessage;
+  sat_simplemodule->scheduleAfter(86700,timeout);
+  sim->executeNextEvent();
+  auto msg = new cMessage;
+  cChannel::Result res = downlink_chl->public_processMessage(msg);
+  sim->run();
+  ASSERT_EQ(res.discard, false);
+  simtime_t delay = downlink_chl->par("distance").doubleValue()/downlink_chl->par("speed_of_light_in_FS").doubleValue();
+  ASSERT_EQ(res.delay.raw(),delay.raw()); // Comparing the raw int64_t values doesn't lose precision, unlike .dbl().
+}
+TEST_F(FSChannelTest, messageFollowingDayNonVisible) {
+  auto timeout = new cMessage;
+  sat_simplemodule->scheduleAfter(86801,timeout);
+  sim->executeNextEvent();
+  auto msg = new cMessage;
+  cChannel::Result res = downlink_chl->public_processMessage(msg);
+  sim->run();
+  ASSERT_EQ(res.discard, true);
+}
 }  // namespace
