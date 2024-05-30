3ffe23adc0307a69aec49eda22044dcc4975448f
Paolo Fittipaldi <paolo.fittipaldi@lip6.fr>
2024-05-30 13:26:07 +0000

    First freespace channels experiment

---
+++ quisp/channels/FSChannel.h
@@ -0,0 +1,35 @@
+/*
+ * FSChannel.h
+ *
+ *  Created on: Sep 6, 2023
+ *      Author: paolo
+ */
+
+#ifndef CHANNELS_FSCHANNEL_H_
+#define CHANNELS_FSCHANNEL_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+namespace channels {
+class FSChannel : public cDatarateChannel {
+ public:
+    FSChannel();
+    void setLOS(bool line_of_sight);
+    void toggleLOS();
+    bool getLOS();
+
+
+ protected:
+  virtual void initialize() override;
+  cChannel::Result processMessage(cMessage *msg, const SendOptions &options, simtime_t t) override;
+
+ private:
+    bool line_of_sight;
+};
+};
+
+
+
+#endif /* CHANNELS_FSCHANNEL_H_ */
