177b2b042cc8eaae1c1ea57c923295ddb8891360
Paolo Fittipaldi <Paolo.Fittipaldi@lip6.fr>
2024-06-07 07:28:00 +0000

    First PointingSystem experiment. This preliminary implementation used to manually set the visibility parameters of free space channels according to a duty cycle. This is not the definitive implementation and it does not work with the current FreeSpaceChannels, but it I left it here for documenting purposes.

---
+++ quisp/modules/Common/GatedQueue.cc
@@ -0,0 +1,105 @@
+
+#include "GatedQueue.h"
+
+
+namespace quisp::modules {
+
+Define_Module(GatedQueue);
+
+
+
+void GatedQueue::initialize() {
+   Queue::initialize();
+   is_busy = false;
+}
+
+void GatedQueue::startTransmitting(cMessage *msg) {
+
+//Find the proper place to do this modification, it's terrible practice to do it here.
+  if (BSMTimingNotification* btn = dynamic_cast<BSMTimingNotification*>(msg)) {
+      if (btn->getFirstPhotonEmitTime() < simTime()) btn->setFirstPhotonEmitTime(simTime()+next_check_time);
+  }
+  Queue::startTransmitting(msg);
+}
+
+void GatedQueue::handleMessage(cMessage *msg)
+{
+    if (hasGUI()) {
+        bubble("GatedQueue received a message!\n");
+      }
+
+    if (auto vco = dynamic_cast<VisCheckOutcome *>(msg)) {
+           if (vco->getNext_check_time() == 0) {
+               pending_vcr = false;
+               msg = (cMessage *)queue.pop();
+               emit(queuing_time_signal, simTime() - msg->getTimestamp());
+               emit(qlen_signal, queue.getLength());
+               startTransmitting(msg);
+           } else {
+               VisCheckRetry* retry = new VisCheckRetry();
+               next_check_time = vco->getNext_check_time();
+               scheduleAfter(next_check_time,retry);
+           }
+          delete vco;
+           return;
+        }
+
+    if (dynamic_cast<VisCheckRetry *>(msg)) {
+        VisCheckRequest* vis_check = new VisCheckRequest();
+        vis_check->setOut_gate(gate("line$o")->getNextGate()->getName());
+        vis_check->setIndex(gate("line$o")->getNextGate()->getIndex());
+        send(vis_check,"to_ps");
+        return;
+    }
+
+    if (msg->arrivedOn("line$i")) {
+        emit(rx_bytes_signal, (long)check_and_cast<cPacket *>(msg)->getByteLength());
+        send(msg, "out");
+        return;
+      }
+
+
+
+
+    if (msg == end_transmission_event) {  // update busy status
+        // Transmission finished, we can start next one.
+        EV_INFO << "Transmission finished.\n";
+        is_busy = false;
+
+        if (queue.isEmpty()) {
+          emit(busy_signal, false);
+          return;
+        }
+
+        if (!is_busy and !queue.isEmpty()) {
+        is_busy = true;
+        VisCheckRequest* vis_check = new VisCheckRequest();
+        vis_check->setOut_gate(gate("line$o")->getNextGate()->getName());
+        vis_check->setIndex(gate("line$o")->getNextGate()->getIndex());
+        send(vis_check,"to_ps");
+        }
+    return;
+    }
+
+    if (frame_capacity && queue.getLength() >= frame_capacity) {
+          EV_INFO << "Received " << msg << " but transmitter busy and queue full: discarding\n";
+          emit(drop_signal, (long)check_and_cast<cPacket *>(msg)->getByteLength());
+          delete msg;
+          return;
+        }
+
+    EV_INFO << "Received " << msg << ": queuing up\n";
+    msg->setTimestamp();
+    queue.insert(msg);
+    emit(qlen_signal, queue.getLength());
+
+    if (!is_busy and !queue.isEmpty() and !pending_vcr) {
+    pending_vcr = true;
+    VisCheckRequest* vis_check = new VisCheckRequest();
+    vis_check->setOut_gate(gate("line$o")->getNextGate()->getName());
+    vis_check->setIndex(gate("line$o")->getNextGate()->getIndex());
+    send(vis_check,"to_ps");
+    }
+}
+
+} //namespace
---
+++ quisp/modules/Common/GatedQueue.h
@@ -0,0 +1,34 @@
+/**
+* \file GatedQueue.h
+*
+* \brief Queue, modified to query the pointing system before sending messages to the outside.
+*
+*/
+
+#ifndef __QUISP_GATEDQUEUE_H_
+#define __QUISP_GATEDQUEUE_H_
+
+#include <omnetpp.h>
+#include "Queue.h"
+#include "messages/visibility_messages_m.h"
+#include "messages/link_generation_messages_m.h"
+
+using namespace omnetpp;
+using namespace quisp::messages;
+
+namespace quisp::modules {
+
+class GatedQueue : public Queue
+{
+  protected:
+    simtime_t next_check_time = 0;
+    virtual void handleMessage(cMessage *msg) override;
+    void initialize() override;
+    void startTransmitting(cMessage *msg) override;
+  private:
+    bool pending_vcr = false;
+};
+
+} //namespace
+
+#endif
