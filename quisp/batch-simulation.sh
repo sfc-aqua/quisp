993b468a6dd58de237f3ce486a88d5131217b897
soon <kento.soon@gmail.com>
2024-07-26 13:53:42 +0000

    make experiment 3 work by editing generator python and ned file, also add sh file to run experiments (old are comment outed)

--- quisp/batch-simulation.sh
+++ quisp/batch-simulation.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-for i in 1 2 4 8 16 32 64;
+# for i in 1 2 4 8 16 32 64;
+# do
+#     echo "Welcome $i times"
+#     ./quisp -n "./networks:./channels:./modules:./simulations" -i \
+#         ./images ./simulations/mim_generation_test.ini -u Cmdenv -c "memory_$i" --cmdenv-status-frequency=10s > "out-$i" &
+# done
+
+# for i in 0 1 2 3 4 5 6 7 8 9 10;
+# do
+#     alicedist="$((20 - $i))"
+#     echo "Welcome $i times $alicedist"
+#     ./quisp -n "./networks:./channels:./modules:./simulations" -i \
+#         ./images ./simulations/cross_validation.ini -u Cmdenv -c "mim_imbalanced_${alicedist}_${i}" --cmdenv-status-frequency=10s > "exp2-raw-${alicedist}_${i}" &
+# done
+
+for cnot in 0 025 05 075 1 125 15 175 2 225 25 275 3 325 35 375 4 425 45 475;
 do
-    echo "Welcome $i times"
+    echo "Welcome cnot: $cnot, meas $meas, without decoherence"
     ./quisp -n "./networks:./channels:./modules:./simulations" -i \
-        ./images ./simulations/mim_generation_test.ini -u Cmdenv -c "memory_$i" --cmdenv-status-frequency=10s > "out-$i" &
+        ./images ./simulations/cross_validation_config_generated.ini -u Cmdenv -c "swapping_validation_cnot_${cnot}_meas_0_with_1ms_decoherence" --cmdenv-status-frequency=10s > "exp3-raw-${cnot}_0_with_1ms_decoherence" &
+
+    echo "Welcome cnot: $cnot, meas $meas, with 1ms decoherence"
+    ./quisp -n "./networks:./channels:./modules:./simulations" -i \
+        ./images ./simulations/cross_validation_config_generated.ini -u Cmdenv -c "swapping_validation_cnot_${cnot}_meas_0_without_decoherence" --cmdenv-status-frequency=10s > "exp3-raw-${cnot}_0_without_decoherence" &
 done
 
-for i in 0 1 2 3 4 5 6 7 8 9 10;
+for meas in 025 05 075 1 125 15 175 2 225 25 275 3 325 35 375 4 425 45 475;
 do
-    alicedist="$((20 - $i))"
-    echo "Welcome $i times $alicedist"
+    echo "Welcome cnot: $cnot, meas $meas, without decoherence"
     ./quisp -n "./networks:./channels:./modules:./simulations" -i \
-        ./images ./simulations/cross_validation.ini -u Cmdenv -c "mim_imbalanced_${alicedist}_${i}" --cmdenv-status-frequency=10s > "exp2-raw-${alicedist}_${i}" &
+        ./images ./simulations/cross_validation_config_generated.ini -u Cmdenv -c "swapping_validation_cnot_0_meas_${meas}_with_1ms_decoherence" --cmdenv-status-frequency=10s > "exp3-raw-0_${meas}_with_1ms_decoherence" &
+
+    echo "Welcome cnot: $cnot, meas $meas, with 1ms decoherence"
+    ./quisp -n "./networks:./channels:./modules:./simulations" -i \
+        ./images ./simulations/cross_validation_config_generated.ini -u Cmdenv -c "swapping_validation_cnot_0_meas_${meas}_without_decoherence" --cmdenv-status-frequency=10s > "exp3-raw-0_${meas}_without_decoherence" &
+
 done
--- quisp/simulations/cross_validation_config_generator.py
+++ quisp/simulations/cross_validation_config_generator.py
@@ -38,12 +38,24 @@ def generate_swapping_config(
     # **.qrsa.hm.initial_purification = 0
     # **.qrsa.hm.purification_type = ""
     # **.buffers = 1
+
+    # fix the cnot_error_prob and measurement_error_prob up to necessary decimal places
+    cnot_error_prob = round(cnot_error_prob, 3)
+    measurement_error_prob = round(measurement_error_prob, 3)
+    # delete the leading 0. and change it into string
+    cnot_error_prob_str = str(cnot_error_prob)[2:]
+    measurement_error_prob_str = str(measurement_error_prob)[2:]
+    if len(cnot_error_prob_str) == 0:
+        cnot_error_prob_str = "0"
+    if len(measurement_error_prob_str) == 0:
+        measurement_error_prob_str = "0"
+
     if with_depolarizing:
-        config_name = f"[Config swapping_validation_cnot_{cnot_error_prob}_meas_{measurement_error_prob}_with_1ms_decoherence]"
+        config_name = f"[Config swapping_validation_cnot_{cnot_error_prob_str}_meas_{measurement_error_prob_str}_with_1ms_decoherence]"
         # p = 0.0004623208506703652 --> resulting in P_I = 0.36781054 after 1ms
         p_decoherence = 0.0004623208506703652
     else:
-        config_name = f"[Config swapping_validation_cnot_{cnot_error_prob}_meas_{measurement_error_prob}_without_decoherence]"
+        config_name = f"[Config swapping_validation_cnot_{cnot_error_prob_str}_meas_{measurement_error_prob_str}_without_decoherence]"
         p_decoherence = 0
     network_name = f"network = networks.cross_validation_swapping"
     error_params = [
@@ -66,7 +78,12 @@ def generate_swapping_config(
         f"**.memory_y_error_rate = {p_decoherence}",
         f"**.memory_z_error_rate = {p_decoherence}",
     ]
-    return [config_name, network_name, *error_params]
+    other_params = [
+        f"**.qrsa.hm.link_tomography = true",
+        f"**.qrsa.hm.initial_purification = 0",
+        f"**.qrsa.hm.purification_type = \"\"",
+    ]
+    return [config_name, network_name, *error_params, *other_params]
 
 
 def write_config(filename: str, configs: list[list[str]]):
@@ -98,6 +115,10 @@ swapping_configs_meas_error = [
 swapping_configs_meas_error_with_depo = [
     generate_swapping_config(0, gp, True) for gp in np.arange(0, 0.5, 0.025)
 ]
+
+# delete the duplicate config in the lists, "swapping_validation_cnot_0_meas_0_"
+swapping_configs_gate_error = swapping_configs_gate_error[1:]
+swapping_configs_gate_error_with_depo = swapping_configs_gate_error_with_depo[1:]
 
 write_config(
     "cross_validation_config_generated.ini",
