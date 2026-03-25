fb02b93a542084afe0e83cb9bdd6565c6b374067
naphann <whit3z@sfc.wide.ad.jp>
2026-03-25 12:23:37 +0000

    fix generate rate without hardcode.

--- quisp/modules/PhysicalConnection/BSA/BSAController.cc
+++ quisp/modules/PhysicalConnection/BSA/BSAController.cc
@@ -6,6 +6,7 @@
 
 #include <cstring>
 #include <stdexcept>
+#include "BellStateAnalyzer.h"
 #include "messages/BSA_ipc_messages_m.h"
 #include "messages/base_messages_m.h"
 #include "messages/link_generation_messages_m.h"
@@ -40,10 +41,12 @@ void BSAController::initialize() {
   time_out_count = 0;
   time_out_message = new BSMNotificationTimeout("bsm_notification_timeout");
   if (is_active) {
-    time_interval_between_photons = SimTime(1, SIMTIME_S) / SimTime(getParentModule()->getSubmodule("bsa")->par("photon_detection_per_second").intValue(), SIMTIME_S);
-    // TODO: hardcoded the interval here for now. to be fixed.
-    time_interval_between_photons = SimTime(1, SIMTIME_NS);
-    std::cout << "time interval: " << time_interval_between_photons << "\n";
+    long long photon_per_second = getParentModule()->getSubmodule("bsa")->par("photon_detection_per_second").intValue();
+    long long time_between_photon_in_atto = ((long long) 1000000000000000000) / photon_per_second;
+    time_interval_between_photons = SimTime(time_between_photon_in_atto, SIMTIME_AS);
+    std::cout << "raw rate given: " << getParentModule()->getSubmodule("bsa")->par("photon_detection_per_second").intValue() << " hi hiiii \n";
+    std::cout << "interval in attosecond " << time_between_photon_in_atto << " atto.s\n";
+    std::cout << "time interval: " << time_interval_between_photons << " hi hiiii \n";
     simtime_t first_notification_timer = SimTime(par("initial_notification_timing_buffer").doubleValue());
     right_qnic = getExternalQNICInfoFromPort(1);
     offset_time_for_first_photon = calculateOffsetTimeFromDistance();
